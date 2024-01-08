952934af9c7f54043754d65f0d8b0d96eeaf34c7
ji-jjang <jinyhehe@gmail.com>
2024-01-09 07:47:59 +0900

    solve kmp

---
+++ src/main/C++/boj/16916_부분문자열.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> failure(string& s) {
+	vector<int> f(s.size());
+	int j = 0;
+	for (int i = 1; i < s.size(); ++i) {
+		while (j > 0 && s[i] != s[j]) j = f[j - 1];
+		if (s[i] == s[j]) f[i] = ++j;
+	}
+	return f;
+}
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	string s, p;
+	cin >> s >> p;
+	vector<int> f = failure(p);
+	int j = 0;
+	for (int i = 0; i < s.size(); ++i) {
+		while (j > 0 && s[i] != p[j]) j = f[j - 1];
+		if (s[i] == p[j]) ++j;
+		if (j == p.size()) {
+			cout << 1;
+			return 0;
+		}
+	}
+	cout << 0;
+}
