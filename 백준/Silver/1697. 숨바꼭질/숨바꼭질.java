3cd9d69549c1163b2f3c8368887e002f0fe13e74
ji-jjang <153375785+ji-jjang@users.noreply.github.com>
2024-08-05 23:10:32 +0900

    [Silver I] Title: 숨바꼭질, Time: 136 ms, Memory: 22068 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+
+		boolean isVisited[] = new boolean[200_001];
+
+		Queue<int[]> q = new LinkedList<>();
+		q.offer(new int[]{n, 0});
+		int ans = Integer.MAX_VALUE;
+		while (!q.isEmpty()) {
+			var cur = q.poll();
+			int y = cur[0];
+			int time = cur[1];
+			if (y == k) {
+				ans = Math.min(ans, time);
+				break; 
+			}
+			if (y - 1 >= 0 && !isVisited[y - 1]) {
+				isVisited[y - 1] = true;
+				q.offer(new int[]{y - 1, time + 1});
+			}
+			if (y + 1 <= 150_000 && !isVisited[y + 1]) {
+				isVisited[y + 1] = true;
+				q.offer(new int[]{y + 1, time + 1});
+			}
+			if (y * 2 <= 150_000 && !isVisited[y * 2]) {
+				isVisited[y * 2] = true;
+				q.offer(new int[]{y * 2, time + 1});
+			}
+		}
+		System.out.println(ans);
+	}
+}
