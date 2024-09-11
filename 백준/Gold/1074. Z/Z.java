ae2fe703f97af03296ca5033f77d9b5903b97550
ji-jjang <153375785+ji-jjang@users.noreply.github.com>
2024-09-11 23:30:50 +0900

    [Gold V] Title: Z, Time: 104 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Gold/1074. Z/Z.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+				StringTokenizer st = new StringTokenizer(br.readLine());
+
+				int n = Integer.parseInt(st.nextToken());
+				int r = Integer.parseInt(st.nextToken());
+				int c = Integer.parseInt(st.nextToken());
+
+				System.out.println(recur(n, r, c));
+    }
+
+		static int recur(int n, int r, int c) {
+			if (n == 0) return 0;
+
+			int half = 1 << (n - 1);
+			if (r < half && c < half) return recur(n - 1, r, c);
+			else if (r < half && c >= half) return half * half + recur(n - 1, r, c - half);
+			else if (r >= half && c < half) return 2 * half * half + recur(n - 1, r - half, c);
+			return 3 * half * half + recur(n - 1, r - half, c - half);
+		}
+}
