aa80d4543b7e3c06d9a1cb9de8623e72962b60c6
ji-jjang <153375785+ji-jjang@users.noreply.github.com>
2024-10-07 23:05:54 +0900

    [Silver III] Title: N과 M (1), Time: 2768 ms, Memory: 217372 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+
+		int[] nums = new int[n + 1];
+		boolean[] isSelected = new boolean[n + 1];
+
+		dfs(0, n, m, nums, isSelected);
+	}
+
+	static void dfs(int depth, int n, int m, int[] nums, boolean[] isSelected) {
+
+		if (depth == m) {
+			for (int i = 0; i < m; ++i) {
+				System.out.printf(nums[i] + " ");
+			}
+			System.out.println();
+			return;
+		}
+		for (int i = 1; i <= n; ++i) {
+			if (isSelected[i])
+				continue;
+			isSelected[i] = true;
+			nums[depth] = i;
+			dfs(depth + 1, n, m, nums, isSelected);
+			isSelected[i] = false;
+		}
+	}
+}
