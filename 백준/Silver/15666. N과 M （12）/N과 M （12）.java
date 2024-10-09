6f7918f4bcd4fe45445e732774f161a6827f22a2
ji-jjang <153375785+ji-jjang@users.noreply.github.com>
2024-10-10 01:23:36 +0900

    [Silver II] Title: N과 M (12), Time: 168 ms, Memory: 20528 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int[] nums = new int[n];
+		int[] seq = new int[m + 1];
+		boolean[] isSelected = new boolean[n];
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; ++i) {
+			nums[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(nums);
+
+		StringBuilder sb = new StringBuilder();
+		Set<String> hs = new LinkedHashSet<>(); 
+		dfs(0, 0, n, m, nums, seq, sb, isSelected, hs);
+		Iterator<String> it = hs.iterator();
+		sb.setLength(0);
+		while (it.hasNext()) {
+			sb.append(it.next());
+			sb.append("\n");
+		}
+		System.out.println(sb);
+	}
+
+	public static void dfs(int depth, int st, int n, int m, int[] nums, int[] seq, StringBuilder sb, boolean[] isSelected, Set<String> hs) {
+
+		if (depth == m) {
+			for (int i = 0; i < m; ++i) {
+				sb.append(seq[i] + " ");
+			}
+			hs.add(sb.toString());
+			sb.setLength(0);
+			return;
+		}
+
+		for (int i = st; i < n; ++i) {
+			seq[depth] = nums[i];
+			dfs(depth + 1, i, n, m, nums, seq, sb, isSelected, hs);
+		}
+	}
+}
