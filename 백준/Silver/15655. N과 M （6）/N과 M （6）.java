e25bf85e0ae8485f509998ae931b04203c6a8f2f
ji-jjang <153375785+ji-jjang@users.noreply.github.com>
2024-10-10 00:45:16 +0900

    [Silver III] Title: N과 M (6), Time: 116 ms, Memory: 15768 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int[] nums = new int[n];
+		boolean[] isSelected = new boolean[n];
+		int[] seq = new int[m + 1];
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; ++i) {
+			nums[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(nums);
+
+		StringBuilder sb = new StringBuilder();
+		dfs(0, n, m, nums, seq, sb, isSelected);
+		System.out.println(sb);
+	}
+
+	public static void dfs(int depth, int n, int m, int[] nums, int[] seq, StringBuilder sb, boolean[] isSelected) {
+
+		if (depth == m) {
+			for (int i = 0; i < m; ++i) {
+				sb.append(seq[i] + " ");
+			}
+			sb.append("\n");
+			return;
+		}
+
+		for (int i = 0; i < n; ++i) {
+			if (isSelected[i] || (depth != 0 && nums[i] < seq[depth - 1])) continue;
+			isSelected[i] = true;
+			seq[depth] = nums[i];
+			dfs(depth + 1, n, m, nums, seq, sb, isSelected);
+			isSelected[i] = false;
+		}
+	}
+}
