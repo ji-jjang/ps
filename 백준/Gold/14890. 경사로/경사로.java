8f432aff9ae5b526041ffb2a4868a5dc6299afd1
ji-jjang <153375785+ji-jjang@users.noreply.github.com>
2024-10-17 08:27:03 +0900

    [Gold III] Title: 경사로, Time: 128 ms, Memory: 15212 KB -BaekjoonHub

--- 백준/Gold/14890. 경사로/경사로.java
+++ 백준/Gold/14890. 경사로/경사로.java
@@ -1,75 +1,80 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int n, l;
-    static int[][] map = new int[104][104];
-    static int ans = 0;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        l = Integer.parseInt(st.nextToken());
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < n; j++)
-                map[i][j] = Integer.parseInt(st.nextToken());
-        }
-        checkRow();
-        rotate();
-        checkRow();
-        System.out.println(ans);
-        br.close();
-    }
-    static void checkRow() {
-        for (int row = 0; row < n; ++row) {
-            int flat = 0;
-            int prev = map[row][0];
-            boolean[] isBridge = new boolean[n];
-            for (int col = 0; col < n; ++col) {
-                if (Math.abs(map[row][col] - prev) > 1)
-                    break;
-                if (prev > map[row][col]) {
-                    int nCol = col;
-                    int nFlat = 0;
-                    int nH = map[row][col];
-                    while (nCol < n) {
-                        if (nFlat == l || nH != map[row][nCol])
-                            break;
-                        isBridge[nCol] = true;
-                        ++nFlat;
-                        ++nCol;
-                    }
-                    if (nFlat < l)
-                        break;
-                    flat = 0;
-                } else if (prev < map[row][col]) {
-                    if (flat < l)
-                        break;
-                    flat = 0;
-                }
-                if (!isBridge[col])
-                    ++flat;
-                prev = map[row][col];
-                if (col == n - 1)
-                    ++ans;
-            }
-        }
-    }
 
-    static void rotate() {
-        int[][] tmp = new int[n][n];
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < n; ++j) {
-                tmp[i][j] = map[i][j];
-            }
-        }
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < n; ++j) {
-                map[j][n - i - 1] = tmp[i][j];
-            }
-        }
-    }
+	static int n, l;
+	static int[][] map;
+	static int ans = 0;
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		l = Integer.parseInt(st.nextToken());
+
+		map = new int[n][n];
+
+		for (int i = 0; i < n; ++i) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < n; ++j) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+
+		for (int i = 0; i < n; ++i) {
+			if (canPassWay(i)) {
+				++ans;
+			}
+		}
+
+		int[][] tmp = new int[n][n];
+		for (int i = 0; i < n; ++i) {
+			for (int j = 0; j < n; ++j) {
+				tmp[j][n - i - 1] = map[i][j];
+			}
+		}
+		for (int i = 0; i < n; ++i) {
+			for (int j = 0; j < n; ++j) {
+				map[i][j] = tmp[i][j];
+			}
+		}
+		for (int i = 0; i < n; ++i) {
+			if (canPassWay(i)) {
+				++ans;
+			}
+		}
+		System.out.println(ans);
+	}
+
+	static boolean canPassWay(int row) {
+
+		boolean[] isUsed = new boolean[n];
+		for (int col = 0; col < n - 1; ++col) {
+			int cur = map[row][col];
+			int nxt = map[row][col + 1];
+			if (cur == nxt)
+				continue;
+			if (Math.abs(cur - nxt) != 1)
+				return false;
+
+			if (cur + 1 == nxt) {
+				for (int i = col; i >= col - l + 1; --i) {
+					if (i < 0 || map[row][i] != cur || isUsed[i])
+						return false;
+					isUsed[i] = true;
+				}
+			}
+			else if (cur - 1 == nxt) { 
+				for (int i = col + 1; i <= col + l; ++i) {
+					if (i >= n || map[row][i] != nxt || isUsed[i])
+						return false;
+					isUsed[i] = true;
+				}
+			}
+		}
+		return true;
+	}
 }
