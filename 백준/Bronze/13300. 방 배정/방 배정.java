03d66a1f12910c9dea4485f468dec3fc78140aff
jinyus <jinyhehe@gmail.com>
2024-10-31 08:23:22 +0900

    [Bronze II] Title: 방 배정, Time: 116 ms, Memory: 14580 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/13300. 방 배정/방 배정.java
+++ 백준/Bronze/13300. 방 배정/방 배정.java
@@ -2,31 +2,34 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+
 	public static void main(String[] args) throws IOException {
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int[][] stu = new int[2][6];
 		StringTokenizer st = new StringTokenizer(br.readLine());
+
 		int n = Integer.parseInt(st.nextToken());
 		int k = Integer.parseInt(st.nextToken());
+
+		int[][] students = new int[2][7]; // gender, grade
+
 		for (int i = 0; i < n; ++i) {
 			st = new StringTokenizer(br.readLine());
 			int gender = Integer.parseInt(st.nextToken());
 			int grade = Integer.parseInt(st.nextToken());
-			stu[gender][grade - 1]++;
+			students[gender][grade]++;
 		}
 
 		int ans = 0;
 		for (int i = 0; i < 2; ++i) {
-			for (int j = 0; j < 6; ++j) {
-				if (stu[i][j] == 0) continue;
-				int share = stu[i][j] / k;
-				int remainder = stu[i][j] % k != 0 ? 1 : 0;
-				ans += (share + remainder);
+			for (int j = 1; j < 7; ++j) {
+				int share = students[i][j] / k;
+				int remainder = students[i][j] % k == 0 ? 0 : 1;
+				ans += share + remainder;
 			}
 		}
-	
 		System.out.println(ans);
-		br.close();
 	}
 }
+
