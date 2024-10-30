1c7f32ce478472a29f96f4a4593a13ba90b93ef3
ji-jjang <153375785+ji-jjang@users.noreply.github.com>
2024-10-30 10:03:15 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 132 ms, Memory: 16152 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
@@ -1,19 +1,20 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
+
 	public static void main(String[] args) throws IOException {
 
-			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-			String str = br.readLine();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-			int[] alpha = new int[26];
-			for (char c : str.toCharArray()) alpha[c - 'a']++;
+		String word = br.readLine();
+		int[] cnt = new int[26];
 
-			for (int i = 0; i < 26; ++i) bw.write(alpha[i] + " ");
-			bw.flush();
-            br.close();
-            bw.close();
+		for (var c : word.toCharArray()) {
+			cnt[c - 'a']++;
+		}
+		for (var e : cnt) {
+			System.out.printf(e + " ");
+		}
 	}
 }
