13d49290d43f850950217d289e9baf6fd846cdbb
ji-jjang <153375785+ji-jjang@users.noreply.github.com>
2024-10-31 08:33:23 +0900

    [Bronze II] Title: Strfry, Time: 256 ms, Memory: 22600 KB -BaekjoonHub

--- 백준/Bronze/11328. Strfry/Strfry.java
+++ 백준/Bronze/11328. Strfry/Strfry.java
@@ -2,30 +2,37 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+
 	public static void main(String[] args) throws IOException {
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		int n = Integer.parseInt(br.readLine());
+
+		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < n; ++i) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 
-			int[] checker = new int[26];
-			char[] str1 = st.nextToken().toCharArray();
-			for (var c : str1) checker[c - 'a']++;
-
-			char[] str2 = st.nextToken().toCharArray();
-			for (var c : str2) checker[c - 'a']--;
-
+			int[] isVisited = new int[26];
 			boolean isStrfry = true;
-			for (int j = 0; j < 26; ++j) if (checker[j] != 0) isStrfry = false;
 
-			String ans = isStrfry ? "Possible\n" : "Impossible\n";
-			bw.write(ans);
+			for (var c : st.nextToken().toCharArray()) isVisited[c - 'a']++;
+			for (var c : st.nextToken().toCharArray()) isVisited[c - 'a']--;
+
+			for (var e : isVisited) {
+				if (e != 0) {
+					isStrfry = false;
+					break;
+				}
+			}
+
+			if (isStrfry) {
+				sb.append("Possible\n");
+				continue;
+			}
+			sb.append("Impossible\n");
 		}
-
-		bw.flush();
-		bw.close();
-		br.close();
+		System.out.println(sb);
 	}
 }
+
