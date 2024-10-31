5fb093f37b85f95e4a7d8aae9bf53093bc1df4c3
ji-jjang <153375785+ji-jjang@users.noreply.github.com>
2024-11-01 08:43:33 +0900

    [Silver II] Title: 에디터, Time: 588 ms, Memory: 147044 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.java
+++ 백준/Silver/1406. 에디터/에디터.java
@@ -1,46 +1,50 @@
 import java.io.*;
-import java.util.LinkedList;
-import java.util.ListIterator;
-import java.util.StringTokenizer;
+import java.util.*;
+
+// ListIterator {e0, e1, e2, e3};
+// cursor       {|e0, e1, e2, e3|};
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        LinkedList<Character> L = new LinkedList<>();
-        String line = br.readLine();
-        for (char c : line.toCharArray()) {
-            L.add(c);
-        }
-        ListIterator<Character> cursor = L.listIterator();
-        while (cursor.hasNext()) {
-            cursor.next();
-        }
-        int n = Integer.parseInt(br.readLine());
-        for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            char op = st.nextToken().toCharArray()[0];
-            if (op == 'L') {
-                if (cursor.hasPrevious())
-                    cursor.previous();
-            } else if (op == 'D') {
-                if (cursor.hasNext())
-                    cursor.next();
-            } else if (op == 'B') {
-                if (cursor.hasPrevious()) {
-                    cursor.previous();
-                    cursor.remove();
-                }
-            } else {
-                char ch;
-                ch = st.nextToken().toCharArray()[0];
-                cursor.add(ch);
-            }
-        }
-        for (char c : L)
-            bw.write(c);
-        bw.flush();
-        bw.close();
-        br.close();
-    }
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		List<Character> l = new LinkedList<>();
+		for (var c : br.readLine().toCharArray()) l.add(c);
+		ListIterator<Character> cursor = l.listIterator(l.size());
+
+		int t = Integer.parseInt(br.readLine());
+		while (t-- > 0) {
+
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			String cmd = st.nextToken();
+
+			if ("L".equals(cmd)) {
+				if (cursor.hasPrevious())
+					cursor.previous();
+			} 
+			else if ("D".equals(cmd)) {
+				if (cursor.hasNext())
+					cursor.next();
+			}
+			else if ("B".equals(cmd)) {
+				if (cursor.hasPrevious()) {
+					cursor.previous();
+					cursor.remove();
+					continue;
+				}
+			} 
+			else {
+				char c = st.nextToken().charAt(0);
+				cursor.add(c);
+			}
+		}
+		StringBuilder sb = new StringBuilder();
+		for (var c : l) {
+			sb.append(c);
+		}
+		System.out.println(sb);
+	}
 }
+
