bcac7cd67409e9825a7c85d2569e48a4434591a3
ji-jjang <153375785+ji-jjang@users.noreply.github.com>
2024-11-01 09:10:14 +0900

    [Silver II] Title: 키로거, Time: 896 ms, Memory: 203072 KB -BaekjoonHub

--- 백준/Silver/5397. 키로거/키로거.java
+++ 백준/Silver/5397. 키로거/키로거.java
@@ -2,39 +2,41 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+
 	public static void main(String[] args) throws IOException {
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		int t = Integer.parseInt(br.readLine());
-
+		StringBuilder sb = new StringBuilder();
 		while (t-- > 0) {
-			String str = br.readLine();
+
 			List<Character> l = new LinkedList<>();
+
 			ListIterator<Character> cursor = l.listIterator();
-			for (var c : str.toCharArray()) {
+
+			for (var c : br.readLine().toCharArray()) {
+
 				if (c == '<') {
-					if (cursor.hasPrevious()) cursor.previous();
-				}
-				else if (c == '>') {
-					if (cursor.hasNext()) cursor.next();
-				}
-				else if (c == '-') {
+					if (cursor.hasPrevious())
+						cursor.previous();
+				} else if (c == '>') {
+					if (cursor.hasNext()) 
+						cursor.next();
+				} else if (c == '-') {
 					if (cursor.hasPrevious()) {
 						cursor.previous();
 						cursor.remove();
 					}
-				}
-				else {
+				} else {
 					cursor.add(c);
 				}
 			}
-			for (var c : l)
-				bw.write(c);
-			bw.write('\n');
+			for (var e : l)
+				sb.append(e);
+			sb.append("\n");
 		}
-		bw.flush();
-		bw.close();
-		br.close();
+		System.out.println(sb);
 	}
 }
+
