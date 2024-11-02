fbe3fa1a56fd0fb242312d3a16876c22df9e4edf
ji-jjang <153375785+ji-jjang@users.noreply.github.com>
2024-11-02 10:10:35 +0900

    [Silver IV] Title: 스택, Time: 200 ms, Memory: 18716 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -2,43 +2,53 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		int n = Integer.parseInt(br.readLine());
 
-		Stack<Integer> s = new Stack<>();
 		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < n; ++i) {
+		Stack<Integer> s = new Stack<>();
+		while (n-- > 0) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			
+
 			String cmd = st.nextToken();
-			if (cmd.equals("push")) {
-				int x = Integer.parseInt(st.nextToken());
-				s.push(x);
-			} else if (cmd.equals("pop")) {
+
+			if ("push".equals(cmd)) {
+
+				s.push(Integer.parseInt(st.nextToken()));
+
+			} else if ("pop".equals(cmd)) {
+
 				if (s.isEmpty()) {
 					sb.append("-1\n");
 					continue;
-				} 
-				sb.append(String.valueOf(s.pop()) + '\n');
-			} else if (cmd.equals("size")) {
-				sb.append(String.valueOf(s.size()) + '\n');
-			} else if (cmd.equals("empty")) {
+				}
+				sb.append(s.pop() + "\n");
+
+			} else if ("size".equals(cmd)) {
+
+				sb.append(s.size() + "\n");
+
+			} else if ("empty".equals(cmd)) {
+
 				if (s.isEmpty()) {
 					sb.append("1\n");
 					continue;
 				}
 				sb.append("0\n");
-			} else if (cmd.equals("top")) {
+
+			} else {
+
 				if (s.isEmpty()) {
 					sb.append("-1\n");
 					continue;
 				}
-				sb.append(String.valueOf(String.valueOf(s.peek()) + '\n'));
+				sb.append(s.peek() + "\n");
 			}
 		}
-		System.out.println(sb.toString());
+		System.out.println(sb);
 	}
 }
