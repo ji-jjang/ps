d37d44f0c7960bb3b46b74cb6b8dd3a888c007cf
ji-jjang <153375785+ji-jjang@users.noreply.github.com>
2024-11-02 10:13:51 +0900

    [Silver IV] Title: 제로, Time: 204 ms, Memory: 23816 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.java
+++ 백준/Silver/10773. 제로/제로.java
@@ -1,25 +1,27 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Stack;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int k = Integer.parseInt(br.readLine());
-        Stack<Integer> s = new Stack<>();
-        int ans = 0;
-        for (int i = 0; i < k; i++) {
-            int num = Integer.parseInt(br.readLine());
-            if (num > 0) {
-                s.push(num);
-                ans += num;
-                continue;
-            }
-            ans -= s.peek();
-            s.pop();
-        }
-        System.out.println(ans);
-        br.close();
-    }
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(br.readLine());
+
+		int ret = 0;
+		Stack<Integer> s = new Stack<>();
+
+		while (n-- > 0) {
+			int num = Integer.parseInt(br.readLine());
+
+			ret += num;
+			if (num == 0 && !s.isEmpty()) {
+				ret -= s.pop();
+				continue;
+			}
+			s.push(num);
+		}
+		System.out.println(ret);
+	}
 }
