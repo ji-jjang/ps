66ed50c1414fb1158b337ed8002f584bbb8b3c6a
ji-jjang <153375785+ji-jjang@users.noreply.github.com>
2024-11-02 11:42:34 +0900

    [Gold V] Title: 탑, Time: 328 ms, Memory: 27312 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -1,24 +1,37 @@
 import java.io.*;
-import java.util.Stack;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int n = Integer.parseInt(br.readLine());
-        Stack<int[]> s = new Stack<>();
-        s.push(new int[]{Integer.MAX_VALUE, 0});
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= n; i++) {
-            int h = Integer.parseInt(st.nextToken());
-            while (s.peek()[0] < h)
-                s.pop();
-            bw.write(s.peek()[1] + " ");
-            s.push(new int[]{h, i});
-        }
-        bw.flush();
-        bw.close();
-        br.close();
-    }
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+
+		Stack<int[]> s = new Stack<>(); // num, index
+
+		boolean isFirst = true;
+		for (int i = 0; i < n; ++i) {
+			int num = Integer.parseInt(st.nextToken());
+			if (isFirst) {
+				sb.append("0" + " ");
+				isFirst = false;
+				s.push(new int[]{num, i + 1});
+				continue;
+			}
+			while (!s.isEmpty() && s.peek()[0] < num) {
+				s.pop();
+			}
+			if (s.isEmpty()) {
+				sb.append("0" + " ");
+			} else {
+				sb.append(s.peek()[1] + " ");
+			}
+			s.push(new int[]{num, i + 1});
+		}
+		System.out.println(sb);
+	}
 }
