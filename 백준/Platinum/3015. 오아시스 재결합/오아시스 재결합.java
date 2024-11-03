23bac7d77c346472b6073e2e33864b9885254238
ji-jjang <153375785+ji-jjang@users.noreply.github.com>
2024-11-04 08:37:29 +0900

    [Platinum V] Title: 오아시스 재결합, Time: 476 ms, Memory: 61824 KB -BaekjoonHub

--- 백준/Platinum/3015. 오아시스 재결합/오아시스 재결합.java
+++ 백준/Platinum/3015. 오아시스 재결합/오아시스 재결합.java
@@ -2,32 +2,32 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+
 	public static void main(String[] args) throws IOException {
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
 		int n = Integer.parseInt(br.readLine());
-		int[] arr = new int[n];
-		for (int i = 0; i < n; ++i) arr[i] = Integer.parseInt(br.readLine());
-
-		Stack<int[]> s = new Stack<>();
-
+		Stack<int[]> s = new Stack<>(); // h, cnt
+																		//
 		long ans = 0;
-		for (int i = 0; i < n; ++i) {
-				int cnt = 1;
-				while (!s.isEmpty() && s.peek()[0] <= arr[i]) {
-						int[] top = s.pop();
-						ans += top[1];
-						if (top[0] == arr[i]) {
-								cnt += top[1];
-						}
-				}
-				if (!s.isEmpty()) {
-						ans++;
-				}
-				s.push(new int[]{arr[i], cnt});
-		}
+		while (n-- > 0) {
+			int h = Integer.parseInt(br.readLine());
+			int cnt = 1;
+			while (!s.isEmpty() && s.peek()[0] <= h) {
 
+				ans += s.peek()[1];
+				if (s.peek()[0] == h) {
+					cnt += s.pop()[1];
+				}
+				else
+					s.pop();
+			}
+			if (!s.isEmpty()) {
+				++ans;
+			}
+			s.push(new int[]{h, cnt});
+		}
 		System.out.println(ans);
-        
-		br.close();
 	}
 }
