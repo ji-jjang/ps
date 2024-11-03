8277d668a0cfd93f932e39d28fc6659234528052
ji-jjang <153375785+ji-jjang@users.noreply.github.com>
2024-11-03 11:30:49 +0900

    [Gold IV] Title: 오큰수, Time: 1088 ms, Memory: 207400 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.java
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -2,26 +2,36 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws IOException {
+BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
 		int n = Integer.parseInt(br.readLine());
-		StringTokenizer st = new StringTokenizer(br.readLine());
+
 		int[] nums = new int[n];
-		for (int i = 0; i < n; ++i) nums[i] = Integer.parseInt(st.nextToken());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		for (int i = 0; i < n; ++i) {
+			nums[i] = Integer.parseInt(st.nextToken());
+		}
 
 		Stack<Integer> s = new Stack<>();
-		StringBuilder sb = new StringBuilder();
-		int[] ans = new int[n];
+		Stack<Integer> track = new Stack<>();
 		for (int i = n - 1; i >= 0; --i) {
-			while (!s.isEmpty() && s.peek() <= nums[i]) s.pop();
-			if (s.isEmpty()) ans[i] = -1;
-			else ans[i] = s.peek();
+
+			while (!s.isEmpty() && s.peek() <= nums[i])
+				s.pop();
+			if (s.isEmpty()) {
+				track.push(-1);
+			} else {
+				track.push(s.peek());
+			}
 			s.push(nums[i]);
 		}
-		for (var e : ans) sb.append(e + " ");
-
-		System.out.println(sb.toString());
-		br.close();
+		StringBuilder sb = new StringBuilder();
+		while (!track.isEmpty()) {
+			sb.append(track.pop() + " ");
+		}
+		System.out.println(sb);
 	}
 }
