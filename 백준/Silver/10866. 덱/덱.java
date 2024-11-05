cd516a7b33fedc3c836a26fc93b4c6722c842776
ji-jjang <153375785+ji-jjang@users.noreply.github.com>
2024-11-06 08:50:48 +0900

    [Silver IV] Title: 덱, Time: 200 ms, Memory: 19848 KB -BaekjoonHub

--- 백준/Silver/10866. 덱/덱.java
+++ 백준/Silver/10866. 덱/덱.java
@@ -1,64 +1,67 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		Deque<Integer> dq = new LinkedList<>();
-		StringBuilder sb = new StringBuilder();
-
 		int n = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		Deque<Integer> dq = new LinkedList<>();
+
 		while (n-- > 0) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
+
 			String cmd = st.nextToken();
-			if (cmd.equals("push_front")) {
-				int num = Integer.parseInt(st.nextToken());
-				dq.offerFirst(num);
-			}
-			else if (cmd.equals("push_back")) {
-				int num = Integer.parseInt(st.nextToken());
-				dq.offerLast(num);
-			}
-			else if (cmd.equals("pop_front")) {
+
+			if (("push_front").equals(cmd)) {
+				dq.offerFirst(Integer.parseInt(st.nextToken()));
+			} else if (("push_back").equals(cmd)) {
+				dq.offerLast(Integer.parseInt(st.nextToken()));
+			} else if (("pop_front").equals(cmd)) {
+
 				if (dq.isEmpty()) {
 					sb.append("-1\n");
-				} else {
-					sb.append(dq.pollFirst() + "\n");
+					continue;
 				}
-			}
-			else if (cmd.equals("pop_back")) {
+				sb.append(dq.pollFirst() + "\n");
+
+			} else if (("pop_back").equals(cmd)) {
+
 				if (dq.isEmpty()) {
 					sb.append("-1\n");
-				} else {
-					sb.append(dq.pollLast() + "\n");
+					continue;
 				}
-			}
-			else if (cmd.equals("size")) {
+				sb.append(dq.pollLast() + "\n");
+
+			} else if (("size").equals(cmd)) {
+
 				sb.append(dq.size() + "\n");
-			}
-			else if (cmd.equals("empty")) {
+
+			} else if (("empty").equals(cmd)) {
+				
 				if (dq.isEmpty()) {
 					sb.append("1\n");
-				} else {
-					sb.append("0\n");
+					continue;
 				}
-			}
-			else if (cmd.equals("front")) {
+				sb.append("0\n");
+
+			} else if (("front").equals(cmd)) {
+
 				if (dq.isEmpty()) {
 					sb.append("-1\n");
-				} else {
-					sb.append(dq.peekFirst() + "\n");
+					continue;
 				}
-			}
-			else if (cmd.equals("back")) {
+				sb.append(dq.getFirst() + "\n");
+
+			} else if (("back").equals(cmd)) {
+
 				if (dq.isEmpty()) {
 					sb.append("-1\n");
-				} else {
-					sb.append(dq.peekLast() + "\n");
+					continue;
 				}
+				sb.append(dq.getLast() + "\n");
 			}
 		}
 		System.out.println(sb);
