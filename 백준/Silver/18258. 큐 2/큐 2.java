20c152596ef264c5bd29ce4028c07cd783009871
ji-jjang <153375785+ji-jjang@users.noreply.github.com>
2024-11-06 08:27:51 +0900

    [Silver IV] Title: 큐 2, Time: 1252 ms, Memory: 347508 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.java
+++ 백준/Silver/18258. 큐 2/큐 2.java
@@ -1,53 +1,45 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		int n = Integer.parseInt(br.readLine());
-		Queue<Integer> q = new LinkedList<>();
 		StringBuilder sb = new StringBuilder();
+		int n = Integer.parseInt(br.readLine());
+
+		Queue<Integer> q = new LinkedList<>();
 		while (n-- > 0) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			String cmd = st.nextToken();
-			if (cmd.equals("push")) {
-				int num = Integer.parseInt(st.nextToken());
-				q.offer(num);
-			}
-			else if (cmd.equals("pop")) {
+			if ("push".equals(cmd)) {
+				q.add(Integer.parseInt(st.nextToken()));
+			} else if ("pop".equals(cmd)) {
 				if (q.isEmpty()) {
 					sb.append("-1\n");
-				}
-				else {
-					sb.append(q.poll() + "\n");
-				}
-			}
-			else if (cmd.equals("size")) {
+					continue;
+				} 
+				sb.append(q.poll() + "\n");
+			} else if ("size".equals(cmd)) {
 				sb.append(q.size() + "\n");
-			}
-			else if (cmd.equals("empty")) {
-					if (q.isEmpty()) {
-						sb.append("1\n");
-					} else {
-						sb.append("0\n");
-					}
-			}
-			else if (cmd.equals("front")) {
+			} else if ("empty".equals(cmd)) {
+				if (q.isEmpty()) {
+					sb.append("1\n");
+					continue;
+				}
+				sb.append("0\n");
+			} else if ("front".equals(cmd)) {
 				if (q.isEmpty()) {
 					sb.append("-1\n");
-				} else {
-					sb.append(q.peek() + "\n");
+					continue;
 				}
-			}
-			else if (cmd.equals("back")) {
+				sb.append(q.peek() + "\n");
+			} else if ("back".equals(cmd)) {
 				if (q.isEmpty()) {
 					sb.append("-1\n");
-				} else {
-					sb.append(((LinkedList<Integer>)q).peekLast() + "\n");
+					continue;
 				}
+				sb.append(((LinkedList<Integer>)q).getLast() + "\n");
 			}
 		}
 		System.out.println(sb);
