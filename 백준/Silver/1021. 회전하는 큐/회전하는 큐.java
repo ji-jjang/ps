09a01be2891901d48678696d10c80482445c9836
ji-jjang <153375785+ji-jjang@users.noreply.github.com>
2024-11-07 08:48:55 +0900

    [Silver III] Title: 회전하는 큐, Time: 104 ms, Memory: 14352 KB -BaekjoonHub

--- 백준/Silver/1021. 회전하는 큐/회전하는 큐.java
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.java
@@ -1,34 +1,36 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		Deque<Integer> dq = new LinkedList<>();
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		LinkedList<Integer> list = new LinkedList<>();
-
 		int n = Integer.parseInt(st.nextToken());
-		for (int i = 1; i <= n; ++i) list.add(i);
-		int m = Integer.parseInt(st.nextToken());
+		for (int i = 1; i <= n; ++i)
+			dq.offerLast(i);
 
+		int t = Integer.parseInt(st.nextToken());
 		st = new StringTokenizer(br.readLine());
 		int ans = 0;
-		for (int i = 0; i < m; ++i) {
-			int num = Integer.parseInt(st.nextToken());
+		while (t-- > 0) {
+			int target = Integer.parseInt(st.nextToken());
 
-			int left = list.indexOf(num);
-			int right = list.size() - left;
-			ans += Math.min(left, right);
+			int index = ((LinkedList<Integer>) dq).indexOf(target);
+			int moveCnt = Math.min(index, dq.size() - index);
+			boolean rotateLeft = (moveCnt == index);
 
-			if (left <= right) {
-				for (int j = 0; j < left; ++j) list.addLast(list.removeFirst());
+			ans += moveCnt;
+			while (moveCnt-- > 0) {
+				if (rotateLeft) {
+					dq.offerLast(dq.pollFirst());
+				} else {
+					dq.offerFirst(dq.pollLast());
+				}
 			}
-			else {
-				for (int j = 0; j < right; ++j) list.addFirst(list.removeLast());
-			}
-			list.removeFirst();
+			dq.pollFirst();
 		}
 		System.out.println(ans);
 	}
