8c93e6472c0a27e132054404d039049cec2322ea
ji-jjang <153375785+ji-jjang@users.noreply.github.com>
2024-11-07 09:39:10 +0900

    [Gold V] Title: AC, Time: 688 ms, Memory: 92092 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.java
+++ 백준/Gold/5430. AC/AC.java
@@ -1,70 +1,71 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
+	public static void main(String[] args) throws IOException {
 
-    public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder ans = new StringBuilder();
+		int t = Integer.parseInt(br.readLine());
 
-        int t = Integer.parseInt(br.readLine());
-        while (t-- > 0) {
-            char[] cmds = br.readLine().toCharArray();
-            br.readLine();
+		StringBuilder sb = new StringBuilder();
+		while (t-- > 0) {
 
-            String line = br.readLine();
-            Deque<Integer> nums = toOnlyNum(line);
+			String cmds = br.readLine();
+			int size = Integer.parseInt(br.readLine());
+			Deque<Integer> dq = new ArrayDeque<>(size);
 
-            boolean isError = false;
-            boolean reversed = false;
-            for (var cmd : cmds) {
-                if (cmd == 'R') {
-                    reversed = !reversed;
-                } else {
-                    if (nums.isEmpty()) {
-                        isError = true;
-                        break;
-                    }
-                    if (reversed) {
-                        nums.removeLast();
-                    } else {
-                        nums.removeFirst();
-                    }
-                }
-            }
-            if (isError) {
-                ans.append("error\n");
-                continue;
-            }
-            ans.append(toCommaNum(nums, reversed));
-        }
-        System.out.println(ans);
-    }
-
-    static Deque<Integer> toOnlyNum(String line) {
-        String[] split = line.substring(1, line.length() - 1).split(",");
-        Deque<Integer> deque = new LinkedList<>();
-        for (String s : split) {
-            if (!s.isEmpty()) {
-                deque.add(Integer.parseInt(s));
-            }
-        }
-        return deque;
-    }
-
-    static StringBuilder toCommaNum(Deque<Integer> nums, boolean reversed) {
-        StringBuilder sb = new StringBuilder();
-				Iterator<Integer> iterator = reversed ? nums.descendingIterator() : nums.iterator();
-
-        sb.append('[');
-				while (iterator.hasNext()) {
-						sb.append(iterator.next());
-						if (iterator.hasNext()) {
-								sb.append(',');
-						}
+			boolean isError = false;
+			String line = br.readLine();
+			if (size != 0) {
+				String[] splited = line.substring(1, line.length() - 1).split(",");
+				for (var e : splited) {
+					dq.offerLast(Integer.parseInt(e));
 				}
-        sb.append("]\n");
-        return sb;
-    }
+			}
+
+			int dir = 1;
+			for (var cmd : cmds.toCharArray()) {
+				if ('R' == cmd) {
+					dir *= -1;
+				}
+				if ('D' == cmd) {
+					if (dq.isEmpty()) {
+						isError = true;
+						break;
+					}
+					if (dir == 1) {
+						dq.pollFirst();
+					} else {
+						dq.pollLast();
+					}
+				}
+			}
+			if (isError) {
+				sb.append("error\n");
+			}
+			else {
+				sb.append("[");
+				int n = dq.size();
+				if (dir == 1) {
+					for (int i = 0; i < n; ++i) {
+						sb.append(dq.getFirst());
+						if (i != n - 1)
+							sb.append(",");
+						dq.pollFirst();
+					}
+				} else {
+					for (int i = 0; i < n; ++i) {
+						sb.append(dq.getLast());
+						if (i != n - 1)
+							sb.append(",");
+						dq.pollLast();
+					}
+
+				}
+				sb.append("]\n");
+			}
+		}
+		System.out.println(sb);
+	}
 }
