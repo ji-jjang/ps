1be1efa890025937f6f83065e1f44df91e95bdfd
ji-jjang <153375785+ji-jjang@users.noreply.github.com>
2024-11-09 20:20:14 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 164 ms, Memory: 18008 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
@@ -1,40 +1,45 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
+
 		while (true) {
 			String line = br.readLine();
-			if (line.equals(".")) break;
-			if (isBalanced(line.toCharArray())) sb.append("yes\n");
-			else sb.append("no\n");
+			if (".".equals(line)) break;
+
+			Deque<Character> dq = new ArrayDeque<>();
+
+			boolean isBalanced = true;
+			for (var c : line.toCharArray()) {
+				if (c == '(' || c == '[') {
+					dq.offerFirst(c);
+				}
+				else if (c == ')') {
+					if (dq.isEmpty() || dq.peekFirst() != '(') {
+						isBalanced = false;
+						break;
+					}
+					dq.pollFirst();
+				}
+				else if (c == ']') {
+					if (dq.isEmpty() || dq.peekFirst() != '[') {
+						isBalanced = false;
+						break;
+					}
+					dq.pollFirst();
+				}
+			}
+			if (!dq.isEmpty()) isBalanced = false;
+			if (isBalanced) {
+				sb.append("yes\n");
+				continue;
+			}
+			sb.append("no\n");
 		}
 		System.out.println(sb);
-	}
-
-	static boolean isBalanced(char[] line) {
-		Stack<Character> s = new Stack<>();
-		for (int i = 0; i < line.length; ++i) {
-			if (line[i] == '(' || line[i] == '[') {
-				s.push(line[i]);
-			}
-			else if (line[i] == ')') {
-				if (s.isEmpty() || s.peek() != '(') {
-					return false;
-				}
-				s.pop();
-			}
-			else if (line[i] == ']') {
-				if (s.isEmpty() || s.peek() != '[') {
-					return false;
-				}
-				s.pop();
-			}
-		}
-		return s.isEmpty();
 	}
 }
