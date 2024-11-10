85acbbc6e9b827a58dadd752960afb254733e17e
ji-jjang <153375785+ji-jjang@users.noreply.github.com>
2024-11-11 08:07:29 +0900

    [Silver IV] Title: 괄호, Time: 104 ms, Memory: 14296 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -2,36 +2,30 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		
+
 		int n = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
 		while (n-- > 0) {
 			String line = br.readLine();
-			if (isCorrect(line)) {
-				sb.append("YES\n");
-				continue;
+			Deque<Character> dq = new ArrayDeque<>();
+			boolean isCorrect = true;
+			for (var c : line.toCharArray()) {
+				if (c == '(') {
+					dq.offerFirst(c);
+				} else {
+					if (dq.isEmpty() || dq.peekFirst() != '(') {
+						isCorrect = false; 
+						break;
+					}
+					dq.pollFirst();
+				}
 			}
-			sb.append("NO\n");
+			if (!dq.isEmpty()) isCorrect = false;
+			sb.append(isCorrect ? "YES\n" : "NO\n");
 		}
 		System.out.println(sb);
-	}
-
-	static boolean isCorrect(String line) {
-		Map<Character, Character> m = new HashMap<>();
-		Stack<Character> s = new Stack<>();
-		m.put(')', '(');
-		for (var c : line.toCharArray()) {
-			if (c == '(') {
-				s.push(c);
-			} else {
-				if (s.isEmpty() || s.peek() != m.get(c)) return false;
-				s.pop();
-			}
-		}
-		return s.isEmpty();
 	}
 }
