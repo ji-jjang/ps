50cd9cec7ae65c006290eb460d0be7f678c0e3b9
ji-jjang <153375785+ji-jjang@users.noreply.github.com>
2024-11-11 09:59:14 +0900

    [Silver I] Title: 미로 탐색, Time: 108 ms, Memory: 14772 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.java
+++ 백준/Silver/1926. 그림/그림.java
@@ -7,49 +7,36 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		StringTokenizer st = new StringTokenizer(br.readLine());
-
 		int r = Integer.parseInt(st.nextToken());
 		int c = Integer.parseInt(st.nextToken());
 
+		int[] dy = {-1, 0, 1, 0};
+		int[] dx = {0, 1, 0, -1};
 		int[][] board = new int[r][c];
 
 		for (int i = 0; i < r; ++i) {
-			st = new StringTokenizer(br.readLine());
+			char[] line = br.readLine().toCharArray();
 			for (int j = 0; j < c; ++j) {
-				board[i][j] = Integer.parseInt(st.nextToken());
+				board[i][j] = line[j] - '0';
 			}
 		}
 
-		int[] dy = {-1, 0, 1, 0};
-		int[] dx = {0, 1, 0, -1};
-		int picCount = 0;
-		int maxPicSize = 0;
 		Queue<int[]> q = new LinkedList<>();
-		for (int i = 0; i < r; ++i) {
-			for (int j = 0; j < c; ++j) {
-				if (board[i][j] == 1) {
-					++picCount;
-					board[i][j] = 2;
-					q.offer(new int[]{i, j});
-				}
-				int cnt = 0;
-				while (!q.isEmpty()) {
-					++cnt;
-					var cur = q.poll();
-					int y = cur[0];
-					int x = cur[1];
-					for (int dir = 0; dir < 4; ++dir) {
-						int ny = y + dy[dir];
-						int nx = x + dx[dir];
-						if (ny < 0 || ny >= r || nx < 0 || nx >= c) continue;
-						if (board[ny][nx] != 1) continue;
-						board[ny][nx] = 2;
-						q.offer(new int[]{ny, nx});
-					}
-				}
-				maxPicSize = Math.max(maxPicSize, cnt);
+		q.offer(new int[]{0, 0});
+		board[0][0] = -1;
+		while (!q.isEmpty()) {
+			var cur = q.poll();
+			int y = cur[0];
+			int x = cur[1];
+			for (int dir = 0; dir < 4; ++dir) {
+				int ny = y + dy[dir];
+				int nx = x + dx[dir];
+				if (ny < 0 || ny >= r || nx < 0 || nx >= c) continue;
+				if (board[ny][nx] != 1) continue;
+				board[ny][nx] = board[y][x] - 1;
+				q.offer(new int[]{ny, nx});
 			}
 		}
-		System.out.println(picCount + "\n" + maxPicSize);
+		System.out.println(board[r - 1][c - 1] * -1);
 	}
 }
