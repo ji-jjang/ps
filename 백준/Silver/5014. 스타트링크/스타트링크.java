423f8e53bce862a94f09e339f789154c8c73fec6
ji-jjang <153375785+ji-jjang@users.noreply.github.com>
2024-11-11 16:52:11 +0900

    [Silver I] Title: 스타트링크, Time: 188 ms, Memory: 62320 KB -BaekjoonHub

--- 백준/Silver/5014. 스타트링크/스타트링크.java
+++ 백준/Silver/5014. 스타트링크/스타트링크.java
@@ -1,8 +1,13 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
+// BFS, O(f)
 public class Main {
+
+	static Queue<int[]> q = new LinkedList<>(); // floor, moveCountjj
+
 	public static void main(String[] args) throws IOException {
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -13,28 +18,34 @@ public class Main {
 		int u = Integer.parseInt(st.nextToken());
 		int d = Integer.parseInt(st.nextToken());
 
-		int[] floor = new int[f + 1];
 		boolean[] isVisited = new boolean[f + 1];
-
-		Queue<int[]> q = new LinkedList<>();
+		boolean isArrive = false;
+		isVisited[s] = true;
 		q.offer(new int[]{s, 0});
+		int ans = 0;
 		while (!q.isEmpty()) {
 			var cur = q.poll();
-			int pos = cur[0];
-			int time = cur[1];
-			if (pos == g) {
-				System.out.println(time);
-				return;
+			int floor = cur[0];
+			int moveCount = cur[1];
+			if (floor == g) {
+				isArrive = true;
+				ans = moveCount;
+				break;
 			}
-			if (pos + u <= f && !isVisited[pos + u]) {
-				isVisited[pos + u] = true;
-				q.offer(new int[]{pos + u, time + 1});
+			
+			if (floor - d >= 1 && !isVisited[floor - d]) {
+				isVisited[floor - d] = true;
+				q.offer(new int[]{floor - d, moveCount + 1});
 			}
-			if (pos - d >= 1 && !isVisited[pos - d]) {
-				isVisited[pos - d] = true;
-				q.offer(new int[]{pos - d, time + 1});
+			if (floor + u <= f && !isVisited[floor + u]) {
+				isVisited[floor + u] = true;
+				q.offer(new int[]{floor + u, moveCount + 1});
 			}
 		}
-		System.out.println("use the stairs");
+		if (!isArrive) {
+			System.out.println("use the stairs");
+			return;
+		}
+		System.out.println(ans);
 	}
 }
