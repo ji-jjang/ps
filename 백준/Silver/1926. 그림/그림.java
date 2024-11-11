36e076ae69b02855ebe19ecf3e8894a5cd4a7dd8
ji-jjang <153375785+ji-jjang@users.noreply.github.com>
2024-11-11 09:38:44 +0900

    [Silver I] Title: 그림, Time: 492 ms, Memory: 46628 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.java
+++ 백준/Silver/1926. 그림/그림.java
@@ -2,56 +2,54 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
-	static int[] dy = {-1, 0, 1, 0};
-	static int[] dx = {0, 1, 0, -1};
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		StringTokenizer st = new StringTokenizer(br.readLine());
+
 		int r = Integer.parseInt(st.nextToken());
 		int c = Integer.parseInt(st.nextToken());
-		int[][] grid = new int[r][c];
-		boolean[][] isVisited = new boolean[r][c];
+
+		int[][] board = new int[r][c];
 
 		for (int i = 0; i < r; ++i) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < c; ++j) {
-				grid[i][j] = Integer.parseInt(st.nextToken());
+				board[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
 
-		int pictureCnt = 0;
-		int maxSize = 0;
+		int[] dy = {-1, 0, 1, 0};
+		int[] dx = {0, 1, 0, -1};
+		int picCount = 0;
+		int maxPicSize = 0;
 		Queue<int[]> q = new LinkedList<>();
 		for (int i = 0; i < r; ++i) {
 			for (int j = 0; j < c; ++j) {
-
-				if (!isVisited[i][j] && grid[i][j] == 1) {
-					++pictureCnt;
-					isVisited[i][j] = true;
+				if (board[i][j] == 1) {
+					++picCount;
+					board[i][j] = 2;
 					q.offer(new int[]{i, j});
-
-					int cnt = 0;
-					while (!q.isEmpty()) {
-						++cnt;
-						maxSize = Math.max(maxSize, cnt);
-						var cur = q.poll();
-						int y = cur[0];
-						int x = cur[1];
-						for (int dir = 0; dir < 4; ++dir) {
-							int ny = y + dy[dir];
-							int nx = x + dx[dir];
-							if (ny < 0 || ny >= r || nx < 0 || nx >= c) continue;
-							if (isVisited[ny][nx] || grid[ny][nx] == 0) continue;
-							isVisited[ny][nx] = true;
-							q.offer(new int[]{ny, nx});
-						}
+				}
+				int cnt = 0;
+				while (!q.isEmpty()) {
+					++cnt;
+					var cur = q.poll();
+					int y = cur[0];
+					int x = cur[1];
+					for (int dir = 0; dir < 4; ++dir) {
+						int ny = y + dy[dir];
+						int nx = x + dx[dir];
+						if (ny < 0 || ny >= r || nx < 0 || nx >= c) continue;
+						if (board[ny][nx] != 1) continue;
+						board[ny][nx] = 2;
+						q.offer(new int[]{ny, nx});
 					}
 				}
+				maxPicSize = Math.max(maxPicSize, cnt);
 			}
 		}
-		System.out.println(pictureCnt + "\n" + maxSize);
+		System.out.println(picCount + "\n" + maxPicSize);
 	}
 }
