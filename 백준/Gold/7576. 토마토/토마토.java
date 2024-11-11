1f9bdcbbfd3423d120d26e5699ac694c4a28fde7
ji-jjang <153375785+ji-jjang@users.noreply.github.com>
2024-11-11 10:17:29 +0900

    [Gold V] Title: 토마토, Time: 608 ms, Memory: 122320 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -2,19 +2,17 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
 	public static void main(String[] args) throws IOException {
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		StringTokenizer st = new StringTokenizer(br.readLine());
-
 		int c = Integer.parseInt(st.nextToken());
 		int r = Integer.parseInt(st.nextToken());
+		int[] dy = {-1, 0, 1, 0};
+		int[] dx = {0, 1, 0, -1};
 
 		int[][] board = new int[r][c];
-		int[][] dist = new int[r][c];
-		for (int i = 0; i < r; ++i) 
-			Arrays.fill(dist[i], -1);
 		Queue<int[]> q = new LinkedList<>();
 
 		for (int i = 0; i < r; ++i) {
@@ -23,12 +21,10 @@ public class Main {
 				board[i][j] = Integer.parseInt(st.nextToken());
 				if (board[i][j] == 1) {
 					q.offer(new int[]{i, j});
-					dist[i][j] = 0;
 				}
 			}
 		}
-		int[] dy = {1, 0, -1, 0};
-		int[] dx = {0, 1, 0, -1};
+
 		while (!q.isEmpty()) {
 			var cur = q.poll();
 			int y = cur[0];
@@ -37,26 +33,22 @@ public class Main {
 				int ny = y + dy[dir];
 				int nx = x + dx[dir];
 				if (ny < 0 || ny >= r || nx < 0 || nx >= c) continue;
-				if (dist[ny][nx] != -1 || board[ny][nx] != 0) continue;
-				dist[ny][nx] = dist[y][x] + 1;
+				if (board[ny][nx] != 0) continue;
+				board[ny][nx] = board[y][x] + 1;
 				q.offer(new int[]{ny, nx});
 			}
 		}
 
-		boolean isImpossible = false;
 		int ans = 0;
 		for (int i = 0; i < r; ++i) {
 			for (int j = 0; j < c; ++j) {
-				if (board[i][j] == 0 && dist[i][j] == -1) {
-					isImpossible = true;
+				if (board[i][j] == 0) {
+					System.out.println(-1);
+					return;
 				}
-				ans = Math.max(ans, dist[i][j]);
+				ans = Math.max(ans, board[i][j]);
 			}
 		}
-		if (isImpossible) {
-			System.out.println(-1);
-			return;
-		}
-		System.out.println(ans);
+		System.out.println(ans - 1);
 	}
 }
