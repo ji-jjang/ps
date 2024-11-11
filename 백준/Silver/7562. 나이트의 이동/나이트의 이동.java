a929a235dca60925f315d0cb4c6f334c764b5be3
ji-jjang <153375785+ji-jjang@users.noreply.github.com>
2024-11-11 14:45:10 +0900

    [Silver I] Title: 나이트의 이동, Time: 348 ms, Memory: 84024 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
@@ -1,26 +1,27 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
+// BFS, O(n^2)
 public class Main {
+	
+	static int[] dy = {-2, -1, 1, 2, 2, 1, -1, -2};
+	static int[] dx = {1, 2, 2, 1, -1, -2, -2, -1};
+	static Queue<int[]> q; // y, x , moveCount
+	static int n;
 	public static void main(String[] args) throws IOException {
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-
-		int[] dy = {-2, -1, 1, 2, 2, 1, -1, -2};
-		int[] dx = {1, 2, 2, 1, -1, -2, -2, -1};
 		int t = Integer.parseInt(br.readLine());
 		while (t-- > 0) {
-			int l = Integer.parseInt(br.readLine());
-			int[][] board = new int[l][l];
-			boolean[][] isVisited = new boolean[l][l];
-			Queue<int[]> q = new LinkedList<>();
-
+			q = new LinkedList<>();
+			int n = Integer.parseInt(br.readLine());
+			int[][] board = new int[n][n];
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			int sy = Integer.parseInt(st.nextToken());
 			int sx = Integer.parseInt(st.nextToken());
-			isVisited[sy][sx] = true;
+			board[sy][sx] = 1;
 			q.offer(new int[]{sy, sx, 0});
-
 			st = new StringTokenizer(br.readLine());
 			int ey = Integer.parseInt(st.nextToken());
 			int ex = Integer.parseInt(st.nextToken());
@@ -29,18 +30,18 @@ public class Main {
 				var cur = q.poll();
 				int y = cur[0];
 				int x = cur[1];
-				int move = cur[2];
+				int moveCount = cur[2];
 				if (y == ey && x == ex) {
-					sb.append(move + "\n");
+					sb.append(moveCount + "\n");
 					break;
 				}
 				for (int dir = 0; dir < 8; ++dir) {
 					int ny = y + dy[dir];
 					int nx = x + dx[dir];
-					if (ny < 0 || ny >= l || nx < 0 || nx >= l) continue;
-					if (isVisited[ny][nx]) continue;
-					isVisited[ny][nx] = true;
-					q.offer(new int[]{ny, nx, move + 1});
+					if (ny < 0 || ny >= n || nx < 0 || nx >= n) continue;
+					if (board[ny][nx] != 0) continue;
+					board[ny][nx] = 1;
+					q.offer(new int[]{ny, nx, moveCount + 1});
 				}
 			}
 		}
