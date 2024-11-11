adddf8356a497d29ede573e7a9862f0dbb935ead
ji-jjang <153375785+ji-jjang@users.noreply.github.com>
2024-11-11 12:09:28 +0900

    [Silver II] Title: 유기농 배추, Time: 148 ms, Memory: 16196 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -1,23 +1,20 @@
 import java.io.*;
 import java.util.*;
 
+// BFS, O(n)
 public class Main {
-
 	public static void main(String[] args) throws IOException {
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int t = Integer.parseInt(br.readLine());
-
+		int T = Integer.parseInt(br.readLine());
 		StringBuilder sb = new StringBuilder();
-		while (t-- > 0) {
+		while (T-- > 0) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			int c = Integer.parseInt(st.nextToken());
 			int r = Integer.parseInt(st.nextToken());
 			int k = Integer.parseInt(st.nextToken());
-
-			int[][]board = new int[r][c];
-			boolean[][] isVisited = new boolean[r][c];
-
+			int[][] board = new int[r][c];
 			while (k-- > 0) {
 				st = new StringTokenizer(br.readLine());
 				int x = Integer.parseInt(st.nextToken());
@@ -25,32 +22,33 @@ public class Main {
 				board[y][x] = 1;
 			}
 			Queue<int[]> q = new LinkedList<>();
-			int ans = 0;
-			int[] dy = {1, 0, -1, 0};
+			
+			int cnt = 0;
+			int[] dy = {-1, 0, 1, 0};
 			int[] dx = {0, 1, 0, -1};
-
 			for (int i = 0; i < r; ++i) {
 				for (int j = 0; j < c; ++j) {
-					if (board[i][j] == 1 && !isVisited[i][j]) {
-						++ans;
-						isVisited[i][j] = true;
+					if (board[i][j] == 1) {
+						board[i][j] = 2;
 						q.offer(new int[]{i, j});
+						++cnt;
 					}
 					while (!q.isEmpty()) {
 						var cur = q.poll();
-
+						int y = cur[0];
+						int x = cur[1];
 						for (int dir = 0; dir < 4; ++dir) {
-							int ny = cur[0] + dy[dir];
-							int nx = cur[1] + dx[dir];
+							int ny = y + dy[dir];
+							int nx = x + dx[dir];
 							if (ny < 0 || ny >= r || nx < 0 || nx >= c) continue;
-							if (board[ny][nx] == 0 || isVisited[ny][nx]) continue;
-							isVisited[ny][nx] = true;
+							if (board[ny][nx] != 1) continue;
+							board[ny][nx] = 2;
 							q.offer(new int[]{ny, nx});
 						}
 					}
 				}
 			}
-			sb.append(ans + "\n");
+			sb.append(cnt + "\n");
 		}
 		System.out.println(sb);
 	}
