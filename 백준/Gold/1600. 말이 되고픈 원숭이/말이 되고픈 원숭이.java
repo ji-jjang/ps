960bffd1da7b5b4375f5f96f8dfa46ddadd44f11
ji-jjang <153375785+ji-jjang@users.noreply.github.com>
2024-11-12 16:31:58 +0900

    [Gold III] Title: 말이 되고픈 원숭이, Time: 564 ms, Memory: 94916 KB -BaekjoonHub

--- 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
+++ 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
@@ -1,17 +1,19 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
+// BFS, O(r * c * k)
 public class Main {
 
 	static int[] dy = {-1, 0, 1, 0};
 	static int[] dx = {0, 1, 0, -1};
-	static int[] hy = {-1, -2, -2, -1, 1, 2, 2, 1};
-	static int[] hx = {-2, -1, 1, 2, 2, 1, -1, -2};
-
+	static int[] hy = {-2, -1, 1, 2, 2, 1, -1, -2};
+	static int[] hx = {1, 2, 2, 1, -1, -2, -2, -1};
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
 		int k = Integer.parseInt(br.readLine());
+
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int c = Integer.parseInt(st.nextToken());
 		int r = Integer.parseInt(st.nextToken());
@@ -19,52 +21,47 @@ public class Main {
 		int[][][] dist = new int[r][c][k + 1];
 
 		for (int i = 0; i < r; ++i) {
-			for (int j = 0; j < c; ++j) {
-				for (int s = 0; s <= k; ++s) {
-					dist[i][j][s] = -1;
-				}
-			}
-		}
-		Queue<int[]> q = new LinkedList<>(); // y, x, horseMoveCnt
-		for (int i = 0; i < r; ++i) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < c; ++j) {
 				board[i][j] = Integer.parseInt(st.nextToken());
+				Arrays.fill(dist[i][j], -1);
 			}
 		}
-		dist[0][0][0] = 0;
-		q.offer(new int[]{0, 0, 0});
 
+		Queue<int[]> q = new LinkedList<>(); // y, x, horseMoveCnt
+		dist[0][0][k] = 0;
+		q.offer(new int[]{0, 0, k});
+		boolean isArrive = false;
 		while (!q.isEmpty()) {
 			var cur = q.poll();
 			int y = cur[0];
 			int x = cur[1];
-			int hCnt = cur[2];
+			int horse = cur[2];
 			if (y == r - 1 && x == c - 1) {
-				System.out.println(dist[y][x][hCnt]);
-				return;
+				isArrive = true;
+				System.out.println(dist[y][x][horse]);
+				break;
 			}
-			
-			if (hCnt < k) {
-				for (int hDir = 0; hDir < 8; ++hDir) {
-					int ny = y + hy[hDir];
-					int nx = x + hx[hDir];
+			if (horse > 0) {
+				for (int dir = 0; dir < 8; ++dir) {
+					int ny = y + hy[dir];
+					int nx = x + hx[dir];
 					if (ny < 0 || ny >= r || nx < 0 || nx >= c) continue;
-					if (board[ny][nx] != 0 || dist[ny][nx][hCnt + 1] != -1) continue;
-					dist[ny][nx][hCnt + 1] = dist[y][x][hCnt] + 1;
-					q.offer(new int[]{ny, nx, hCnt + 1});
+					if (board[ny][nx] == 1 || dist[ny][nx][horse - 1] != -1) continue;
+					dist[ny][nx][horse - 1] = dist[y][x][horse] + 1;
+					q.offer(new int[]{ny, nx, horse - 1});
 				}
 			}
 			for (int dir = 0; dir < 4; ++dir) {
 				int ny = y + dy[dir];
 				int nx = x + dx[dir];
 				if (ny < 0 || ny >= r || nx < 0 || nx >= c) continue;
-				if (board[ny][nx] != 0 || dist[ny][nx][hCnt] != -1) continue;
-				dist[ny][nx][hCnt] = dist[y][x][hCnt] + 1;
-				q.offer(new int[]{ny, nx, hCnt});
+				if (board[ny][nx] == 1 || dist[ny][nx][horse] != -1) continue;
+				dist[ny][nx][horse] = dist[y][x][horse] + 1;
+				q.offer(new int[]{ny, nx, horse});
 			}
 		}
-		System.out.println(-1);
+		if (!isArrive)
+			System.out.println(-1);
 	}
 }
-
