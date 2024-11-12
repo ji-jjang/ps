b4c02326604debd1273e6ce81941ae03b3d2ca05
ji-jjang <153375785+ji-jjang@users.noreply.github.com>
2024-11-12 15:46:48 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 140 ms, Memory: 21836 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -1,40 +1,42 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
+// BFS, O(n * n * n * n)
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int n = Integer.parseInt(st.nextToken());
 		int k = Integer.parseInt(st.nextToken());
-		boolean[] isVisited = new boolean[150_001];
-		Queue<int[]> q = new LinkedList<>();
 
+		boolean[] isVisited = new boolean[150_004];
+		Queue<int[]> q = new LinkedList<>();
 		isVisited[n] = true;
 		q.offer(new int[]{n, 0});
+
 		while (!q.isEmpty()) {
 			var cur = q.poll();
-			int pos = cur[0];
-			int move = cur[1];
-			if (pos == k) {
-				System.out.println(move);
+			int lot = cur[0];
+			int time = cur[1];
+			if (lot == k) {
+				System.out.println(time);
 				break;
 			}
-			if (pos * 2 <= 150_000 && !isVisited[pos * 2]) {
-				isVisited[pos * 2] = true;
-				q.offer(new int[]{pos * 2, move});
+			if (lot * 2 <= 150_000 && !isVisited[lot * 2]) {
+				isVisited[lot * 2] = true;
+				q.offer(new int[]{lot * 2, time});
 			}
-			if (pos - 1 >= 0 && !isVisited[pos - 1]) {
-				isVisited[pos - 1] = true;
-				q.offer(new int[]{pos - 1, move + 1});
+			if (lot - 1 >= 0 && !isVisited[lot - 1]) {
+				isVisited[lot - 1] = true;
+				q.offer(new int[]{lot - 1, time + 1});
 			}
-			if (pos + 1 <= 100_000 && !isVisited[pos + 1]) {
-				isVisited[pos + 1] = true;
-				q.offer(new int[]{pos + 1, move + 1});
+			if (lot + 1 <= 100_000 && !isVisited[lot + 1]) {
+				isVisited[lot + 1] = true;
+				q.offer(new int[]{lot + 1, time + 1});
 			}
 		}
 	}
 }
-
