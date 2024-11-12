a077faa22753caa1c66dee4902bfba4468d97f78
ji-jjang <153375785+ji-jjang@users.noreply.github.com>
2024-11-12 14:23:42 +0900

    [Gold IV] Title: 빙산, Time: 1164 ms, Memory: 261100 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.java
+++ 백준/Gold/2573. 빙산/빙산.java
@@ -1,89 +1,78 @@
 import java.io.*;
 import java.util.*;
 
+// BFS, O(year * r * c)
 public class Main {
-	public static void main(String[] args) throws IOException  {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
 
+	static int[] dy = {-1, 0, 1, 0};
+	static int[] dx = {0, 1, 0, -1};
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		int r = Integer.parseInt(st.nextToken());
 		int c = Integer.parseInt(st.nextToken());
 		int[][] board = new int[r][c];
+
+		int maxHeight = 0;
 		for (int i = 0; i < r; ++i) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < c; ++j) {
 				board[i][j] = Integer.parseInt(st.nextToken());
+				maxHeight = Math.max(maxHeight, board[i][j]);
 			}
 		}
 
-		int[] dy = {-1, 0, 1, 0};
-		int[] dx = {0, 1, 0, -1};
-		int ans = 0;
-		while (true) {
-			boolean isAllMelted = true;
+		for (int year = 1; year <= 4000; ++year) {
 			int[][] tmp = new int[r][c];
-			boolean[][] isVisited = new boolean[r][c];
+			for (int i = 1; i < r - 1; ++i) {
+				for (int j = 1; j < c - 1; ++j) {
+					int cnt = 0;
+					for (int dir = 0; dir < 4; ++dir) {
+						int ny = i + dy[dir];
+						int nx = j + dx[dir];
+						if (board[ny][nx] == 0)
+							++cnt;
+					}
+					tmp[i][j] = board[i][j] - cnt;
+					if (tmp[i][j] < 0) tmp[i][j] = 0;
+				}
+			}
+			for (int i = 0; i < r; ++i) {
+				for (int j = 0; j < c; ++j) {
+					board[i][j] = tmp[i][j];
+				}
+			}
 			Queue<int[]> q = new LinkedList<>();
-
-			for (int i = 0; i < r; ++i) {
-				for (int j = 0; j < c; ++j) {
-					if (board[i][j] != 0) {
-						isAllMelted = false;
-						int zeroCnt = 0;
-						for (int dir = 0; dir < 4; ++dir) {
-							int ni = i + dy[dir];
-							int nj = j + dx[dir];
-							if (ni < 0 || ni >= r || nj < 0 || nj >= c) continue;
-							if (board[ni][nj] != 0) continue;
-							++zeroCnt;
-						}
-						tmp[i][j] = zeroCnt;
-					}
-				}
-			}
-
-			++ans;
-			if (isAllMelted) {
-				ans = 0;
-				break;
-			}
-
-			for (int i = 0; i < r; ++i) {
-				for (int j = 0; j < c; ++j) {
-					board[i][j] = Math.max(0, board[i][j] - tmp[i][j]);
-				}
-			}
-
-			int splitedCnt = 0;
-			for (int i = 0; i < r; ++i) {
-				for (int j = 0; j < c; ++j) {
-					if (board[i][j] != 0 && !isVisited[i][j]) {
-						splitedCnt++;
-						if (splitedCnt > 1) {
-							break;
-						}
-						isVisited[i][j] = true;
+			int cnt = 0;
+			for (int i = 1; i < r - 1; ++i) {
+				for (int j = 1; j < c - 1; ++j) {
+					if (tmp[i][j] > 0) {
+						++cnt;
+						tmp[i][j] = -1;
 						q.offer(new int[]{i, j});
-						while (!q.isEmpty()) {
-							var cur = q.poll();
-							int y = cur[0];
-							int x = cur[1];
-							for (int dir = 0; dir < 4; ++dir) {
-								int ny = y + dy[dir];
-								int nx = x + dx[dir];
-								if (ny < 0 || ny >= r || nx < 0 || nx >= c) continue;
-								if (board[ny][nx] == 0 || isVisited[ny][nx]) continue;
-								isVisited[ny][nx] = true;
-								q.offer(new int[]{ny, nx});
-							}
+					}
+					while (!q.isEmpty()) {
+						var cur = q.poll();
+						int y = cur[0];
+						int x = cur[1];
+						for (int dir = 0; dir < 4; ++dir) {
+							int ny = y + dy[dir];
+							int nx = x + dx[dir];
+							if (ny < 0 || ny >= r || nx < 0 || nx >= c) continue;
+							if (tmp[ny][nx] <= 0) continue;
+							tmp[ny][nx] = -1;
+							q.offer(new int[]{ny, nx});
 						}
 					}
 				}
-				if (splitedCnt > 1) break;
 			}
-
-			if (splitedCnt > 1) break;
+			if (cnt > 1) {
+				System.out.println(year);
+				return;
+			}
 		}
-		System.out.println(ans);
+		System.out.println(0);
 	}
 }
