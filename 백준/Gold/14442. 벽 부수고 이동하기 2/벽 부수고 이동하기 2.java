c583bec8df36c4d4611e406bf5bdd3e530872762
ji-jjang <153375785+ji-jjang@users.noreply.github.com>
2024-11-13 12:26:42 +0900

    [Gold III] Title: 벽 부수고 이동하기 2, Time: 1832 ms, Memory: 408680 KB -BaekjoonHub

--- 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.java
+++ 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.java
@@ -1,66 +1,55 @@
 import java.io.*;
 import java.util.*;
 
+// BFS, O(r * c * k)
 public class Main {
 
+	static int[] dy = {-1, 0, 1, 0};
+	static int[] dx = {0, 1, 0, -1};
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		StringTokenizer st = new StringTokenizer(br.readLine());
-
-		int r = Integer.parseInt(st.nextToken());
-		int c = Integer.parseInt(st.nextToken());
-		int k = Integer.parseInt(st.nextToken());
+		String[] tokens = br.readLine().split(" ");
+		int r = Integer.parseInt(tokens[0]);
+		int c = Integer.parseInt(tokens[1]);
+		int k = Integer.parseInt(tokens[2]);
 
 		int[][] board = new int[r][c];
 		int[][][] dist = new int[r][c][k + 1];
+		
 		for (int i = 0; i < r; ++i) {
+			char[] line = br.readLine().toCharArray();
 			for (int j = 0; j < c; ++j) {
-				for (int l = 0; l <= k; ++l) {
-					dist[i][j][l] = -1;
-				}
+				board[i][j] = line[j] - '0';
+				Arrays.fill(dist[i][j], -1);
 			}
 		}
 
-		for (int i = 0; i < r; ++i) {
-			String line = br.readLine();
-			for (int j = 0; j < c; ++j) {
-				board[i][j] = line.charAt(j) - '0';
-			}
-		}
-		int[] dy = {-1, 0, 1, 0};
-		int[] dx = {0, 1, 0, -1};
-
-		Queue<int[]> q = new LinkedList<>(); // y, x, breakCnt;
-		dist[0][0][0] = 0;
-		q.offer(new int[]{0, 0, 0});
+		Queue<int[]> q = new LinkedList<>(); // y, x, remainBreakCnt
+		dist[0][0][k] = 1;
+		q.offer(new int[]{0, 0, k});
 		while (!q.isEmpty()) {
 			var cur = q.poll();
 			int y = cur[0];
 			int x = cur[1];
 			int cnt = cur[2];
 			if (y == r - 1 && x == c - 1) {
-				System.out.println(dist[y][x][cnt] + 1);
+				System.out.println(dist[y][x][cnt]);
 				return;
 			}
 			for (int dir = 0; dir < 4; ++dir) {
 				int ny = y + dy[dir];
 				int nx = x + dx[dir];
 				if (ny < 0 || ny >= r || nx < 0 || nx >= c) continue;
-				if (board[ny][nx] == 1) {
-					if (cnt >= k)
-						continue;
-					if (dist[ny][nx][cnt + 1] != -1) continue;
-					dist[ny][nx][cnt + 1] = dist[y][x][cnt] + 1;
-					q.offer(new int[]{ny, nx, cnt + 1});
-				} 
-				else {
-					if (dist[ny][nx][cnt] != -1)
-						continue;
-					dist[ny][nx][cnt] = dist[y][x][cnt] + 1;
-					q.offer(new int[]{ny, nx, cnt});
+				if (board[ny][nx] == 1 && cnt > 0) {
+					if (dist[ny][nx][cnt - 1] != -1) continue;
+					dist[ny][nx][cnt - 1] = dist[y][x][cnt] + 1;
+					q.offer(new int[]{ny, nx, cnt - 1});
 				}
+				if (board[ny][nx] == 1 || dist[ny][nx][cnt] != -1) continue;
+				dist[ny][nx][cnt] = dist[y][x][cnt] + 1;
+				q.offer(new int[]{ny, nx, cnt});
 			}
 		}
 		System.out.println(-1);
