6bb0a2aed84b5da190c6bed4eb4b7374a31b57c0
ji-jjang <153375785+ji-jjang@users.noreply.github.com>
2024-11-13 11:18:24 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 340 ms, Memory: 40044 KB -BaekjoonHub

--- 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.java
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.java
@@ -1,53 +1,58 @@
 import java.io.*;
 import java.util.*;
 
+// BFS, O(V)
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		String[] token = br.readLine().split(" ");
+		int n = Integer.parseInt(token[0]);
+		int k = Integer.parseInt(token[1]);
 
-		int n = Integer.parseInt(st.nextToken());
-		int k = Integer.parseInt(st.nextToken());
-
-		Queue<int[]> q = new LinkedList<>(); // pos, time
+		Queue<int[]> q = new LinkedList<>(); // location, time
 		boolean[] isVisited = new boolean[150_001];
 		int[] path = new int[150_001];
+
 		isVisited[n] = true;
 		q.offer(new int[]{n, 0});
-		
+		boolean isArrive = false;
+		int lot, time = 0;
 		while (!q.isEmpty()) {
 			var cur = q.poll();
-			int pos = cur[0];
-			int time = cur[1];
-			if (pos == k) {
-				System.out.println(time);
+			lot = cur[0];
+			time = cur[1];
+			if (lot == k) {
 				break;
 			}
-			if (pos - 1 >= 0 && !isVisited[pos - 1]) {
-				isVisited[pos - 1] = true;
-				path[pos - 1] = pos;
-				q.offer(new int[]{pos - 1, time + 1});
+			if (lot * 2 <= 150_000 && !isVisited[lot * 2]) {
+				path[lot * 2] = lot;
+				isVisited[lot * 2] = true;
+				q.offer(new int[]{lot * 2, time + 1});
 			}
-			if (pos * 2 <= 150_000 && !isVisited[pos * 2]) {
-				isVisited[pos * 2] = true;
-				path[pos * 2] = pos;
-				q.offer(new int[]{pos * 2, time + 1});
+			if (lot + 1 <= 100_000 && !isVisited[lot + 1]) {
+				path[lot + 1] = lot;
+				isVisited[lot + 1] = true;
+				q.offer(new int[]{lot + 1, time + 1});
 			}
-			if (pos + 1 <= 100_000 && !isVisited[pos + 1]) {
-				isVisited[pos + 1] = true;
-				path[pos + 1] = pos;
-				q.offer(new int[]{pos + 1, time + 1});
+			if (lot - 1 >= 0 && !isVisited[lot - 1]) {
+				path[lot - 1] = lot;
+				isVisited[lot - 1] = true;
+				q.offer(new int[]{lot - 1, time + 1});
 			}
 		}
-		int cur = k;
+		List<Integer> ret = new ArrayList<>();
+		for (int i = k; i != n; i = path[i]) {
+			ret.add(i);
+		}
+		ret.add(n);
+		Collections.reverse(ret);
 		StringBuilder sb = new StringBuilder();
-		while (true) {
-			sb.insert(0, cur + " ");
-			if (cur == n) break;
-			cur = path[cur];
+		sb.append(time + "\n");
+		for (var e : ret) {
+			sb.append(e + " ");
 		}
 		System.out.println(sb);
 	}
