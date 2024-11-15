35ab5ffde542b328e35e7b030be534949874c5a5
ji-jjang <153375785+ji-jjang@users.noreply.github.com>
2024-11-15 11:33:46 +0900

    [Gold II] Title: 불켜기, Time: 308 ms, Memory: 34772 KB -BaekjoonHub

--- 백준/Gold/11967. 불켜기/불켜기.java
+++ 백준/Gold/11967. 불켜기/불켜기.java
@@ -1,75 +1,69 @@
 import java.io.*;
 import java.util.*;
 
+// BFS, O(n^2)
 public class Main {
 
+	static int[] dy = {-1, 0, 1, 0};
+	static int[] dx = {0, 1, 0, -1};
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
-		List<List<List<int[]>>> switches = new ArrayList<>();
+		String[] tokens = br.readLine().split(" ");
+		int n = Integer.parseInt(tokens[0]);
+		int m = Integer.parseInt(tokens[1]);
 
-		boolean[][] isVisited = new boolean[n + 1][n + 1];
-		int[][] board = new int[n + 1][n + 1];
+		boolean[][] room = new boolean[n + 2][n + 2]; // true = light on
+		boolean[][] isVisited = new boolean[n + 2][n + 2];
 
-		for (int i = 0; i <= n; ++i) {
-			switches.add(new ArrayList<>());
-			for (int j = 0; j <= n; ++j) {
-				switches.get(i).add(new ArrayList<>());
-			}
-		}
+		Map<List<Integer>, List<int[]>> switches = new HashMap<>();
 		for (int i = 0; i < m; ++i) {
-			st = new StringTokenizer(br.readLine());
-			int y, x, a, b;
-			y = Integer.parseInt(st.nextToken());
-			x = Integer.parseInt(st.nextToken());
-			a = Integer.parseInt(st.nextToken());
-			b = Integer.parseInt(st.nextToken());
-			switches.get(y).get(x).add(new int[]{a, b});
+			tokens = br.readLine().split(" ");
+			int sy = Integer.parseInt(tokens[0]);
+			int sx = Integer.parseInt(tokens[1]);
+			int ey = Integer.parseInt(tokens[2]);
+			int ex = Integer.parseInt(tokens[3]);
+			List<Integer> key = Arrays.asList(sy, sx);
+			switches.putIfAbsent(key, new ArrayList<>());
+			switches.get(key).add(new int[]{ey, ex});
 		}
 
-		int[] dy = {-1, 0, 1, 0};
-		int[] dx = {0, 1, 0, -1};
 		Queue<int[]> q = new LinkedList<>();
-
-		board[1][1] = 1; // light off: 0, light on: 1
 		isVisited[1][1] = true;
+		room[1][1] = true;
 		q.offer(new int[]{1, 1});
+		int ans = 1;
 		while (!q.isEmpty()) {
 			var cur = q.poll();
 			int y = cur[0];
 			int x = cur[1];
-			for (var e : switches.get(y).get(x)) {
-				int yy = e[0];
-				int xx = e[1];
-				if (isVisited[yy][xx]) continue;
-				board[yy][xx] = 1;
-				for (int dir = 0; dir < 4; ++dir) {
-					int nyy = yy + dy[dir];
-					int nxx = xx + dx[dir];
-					if (nyy < 1 || nyy > n || nxx < 1 || nxx > n) continue;
-					if (!isVisited[nyy][nxx]) continue;
-					isVisited[yy][xx] = true;
-					q.offer(new int[]{yy, xx});
-					break;
+			List<int[]> coords = switches.get(Arrays.asList(y, x));
+			if (coords != null) {
+				for (var coord : coords) {
+					int cy = coord[0];
+					int cx = coord[1];
+					if (isVisited[cy][cx]) continue;
+					if (!room[cy][cx]) ++ans;
+					room[cy][cx] = true;
+					for (int dir = 0; dir < 4; ++dir) {
+						int ccy = cy + dy[dir];
+						int ccx = cx + dx[dir];
+						if (ccy < 1 || ccy > n + 1 || ccx < 1 || ccx > n + 1) continue;
+						if (!isVisited[ccy][ccx]) continue;
+						isVisited[cy][cx] = true;
+						q.offer(new int[]{cy, cx});
+						break;
+					}
 				}
 			}
 			for (int dir = 0; dir < 4; ++dir) {
 				int ny = y + dy[dir];
 				int nx = x + dx[dir];
-				if (ny < 1 || ny > n || nx < 1 || nx > n) continue;
-				if (board[ny][nx] != 1 || isVisited[ny][nx]) continue;
+				if (ny < 1 || ny > n + 1 || nx < 1 || nx > n + 1) continue;
+				if (room[ny][nx] != true || isVisited[ny][nx]) continue;
 				isVisited[ny][nx] = true;
 				q.offer(new int[]{ny, nx});
-			}
-		}
-		int ans = 0;
-		for (int i = 1; i <= n; ++i) {
-			for (int j = 1; j <= n; ++j) {
-				if (board[i][j] == 1) ++ans;
 			}
 		}
 		System.out.println(ans);
