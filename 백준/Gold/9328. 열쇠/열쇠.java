21a6c3327b47d5b8fced018b84ca6236b5b9d814
ji-jjang <153375785+ji-jjang@users.noreply.github.com>
2024-11-19 13:18:12 +0900

    [Gold I] Title: 열쇠, Time: 196 ms, Memory: 21608 KB -BaekjoonHub

--- 백준/Gold/9328. 열쇠/열쇠.java
+++ 백준/Gold/9328. 열쇠/열쇠.java
@@ -1,8 +1,11 @@
 import java.io.*;
 import java.util.*;
 
+// BFS, O(t * (r * c + D * L))
 public class Main {
 
+	static int[] dy = {-1, 0, 1, 0};
+	static int[] dx = {0, 1, 0, -1};
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -10,76 +13,71 @@ public class Main {
 		int t = Integer.parseInt(br.readLine());
 		StringBuilder sb = new StringBuilder();
 		while (t-- > 0) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int r = Integer.parseInt(st.nextToken());
-			int c = Integer.parseInt(st.nextToken());
+			Map<Character, List<int[]>> doors = new HashMap<>();
+			Set<Character> keys = new HashSet<>();
+			String[] tokens = br.readLine().split(" ");
+			int r = Integer.parseInt(tokens[0]);
+			int c = Integer.parseInt(tokens[1]);
 
 			char[][] board = new char[r + 2][c + 2];
 			boolean[][] isVisited = new boolean[r + 2][c + 2];
-			for (int i = 1; i <= r; ++i) {
-				String line = br.readLine();
-				for (int j = 1; j <= c; ++j) {
-					board[i][j] = line.charAt(j - 1);
+			for (int i = 0; i < r + 2; ++i) {
+				Arrays.fill(board[i], '.');
+			}
+			for (int i = 1; i < r + 1; ++i) {
+				char[] line = br.readLine().toCharArray();
+				for (int j = 1; j < c + 1; ++j) {
+					board[i][j] = line[j - 1];
 				}
 			}
-			String line = br.readLine();
-
-			Set<Character> keys = new HashSet<>();
-			for (int i = 0; i < line.length(); ++i) {
-				if (line.charAt(i) == '0') break;
-				keys.add(line.charAt(i));
+			char[] ks = br.readLine().toCharArray();
+			for (char key : ks) {
+				if (key == '0') break;
+				keys.add(key);
 			}
 
-			sb.append(bfs(board, r, c , keys, isVisited) + "\n");
-		}
-		System.out.println(sb.toString());
-	}
+			Queue<int[]> q = new LinkedList<>();
+			isVisited[0][0] = true;
+			q.offer(new int[]{0, 0});
+			int ans = 0;
+			while (!q.isEmpty()) {
+				var cur = q.poll();
+				int y = cur[0];
+				int x = cur[1];
+				for (int dir = 0; dir < 4; ++dir) {
+					int ny = y + dy[dir];
+					int nx = x + dx[dir];
 
-	static int bfs(char[][] board, int r, int c, Set<Character> keys, boolean[][] isVisited) {
-		int[] dy = {-1, 0, 1, 0};
-		int[] dx = {0, 1, 0, -1};
-		Queue<int[]> q = new LinkedList<>();
-		Map<Character, List<int[]>> doors = new HashMap<>();
+					if (ny < 0 || ny > r + 1 || nx < 0 || nx > c + 1) continue;
 
-		int ans = 0;
-		q.offer(new int[]{0, 0});
-		isVisited[0][0] = true;
-		while (!q.isEmpty()) {
-			var cur = q.poll();
-			int y = cur[0], x = cur[1];
+					if (isVisited[ny][nx] || board[ny][nx] == '*') continue;
 
-			for (int dir = 0; dir < 4; dir++) {
-				int ny = y + dy[dir];
-				int nx = x + dx[dir];
-
-				if (ny < 0 || ny > r + 1 || nx < 0 || nx > c + 1) continue;
-				if (isVisited[ny][nx] || board[ny][nx] == '*') continue;
-
-				char color = board[ny][nx];
-				isVisited[ny][nx] = true;
-				if (color >= 'a' && color <= 'z') {
-					keys.add(color);
-					char door = (char)(color - 'a' + 'A');
-					if (doors.containsKey(door)) {
-						for (var d : doors.get(door)) q.offer(d);
-						doors.remove(door);
+					isVisited[ny][nx] = true;
+					if (board[ny][nx] >= 'a' && board[ny][nx] <= 'z') {
+						keys.add(board[ny][nx]);
+						char door = (char)(board[ny][nx] - 'a' + 'A');
+						if (doors.containsKey(door)) {
+							for (var pos : doors.get(door))
+								q.offer(new int[]{pos[0], pos[1]});
+							doors.remove(door);
+						}
 					}
-				}
-				else if (color >= 'A' && color <= 'Z') {
-					char key = (char)(color - 'A' + 'a');
-					if (keys.contains(key)) {
-						q.offer(new int[]{ny, nx});
-					} else {
-						doors.putIfAbsent(color, new ArrayList<>());
-						doors.get(color).add(new int[]{ny, nx});
+					else if (board[ny][nx] >= 'A' && board[ny][nx] <= 'Z') {
+						char key = (char)(board[ny][nx] - 'A' + 'a');
+						if (!keys.contains(key)) {
+							doors.putIfAbsent(board[ny][nx], new ArrayList<>());
+							doors.get(board[ny][nx]).add(new int[]{ny, nx});
+							continue;
+						}
 					}
-					continue;
+					else if (board[ny][nx] == '$') {
+						++ans;
+					}
+					q.offer(new int[]{ny, nx});
 				}
-				else if (board[ny][nx] == '$') ++ans;
-				q.offer(new int[]{ny, nx});
 			}
+			sb.append(ans + "\n");
 		}
-		return ans;
+		System.out.println(sb);
 	}
 }
-
