77d629ac64280ca94d6ff433647a119fc2cae0f5
ji-jjang <153375785+ji-jjang@users.noreply.github.com>
2024-11-21 11:06:11 +0900

    [Platinum V] Title: 비밀번호 제작, Time: 472 ms, Memory: 72036 KB -BaekjoonHub

--- 백준/Platinum/20304. 비밀번호 제작/비밀번호 제작.java
+++ 백준/Platinum/20304. 비밀번호 제작/비밀번호 제작.java
@@ -1,48 +1,52 @@
 import java.io.*;
 import java.util.*;
 
+// BFS, O(n)
+// HammingDistance 이용
+// K번째 비트가 0인지 1인지에 따라 비트를 하나씩 변경해주기
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine());
-        int m = Integer.parseInt(br.readLine());
+	public static void main(String[] args) throws IOException {
 
-        int[] dist = new int[n + 1];
-        Arrays.fill(dist, -1);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int m = Integer.parseInt(br.readLine());
 
-        Queue<Integer> q = new LinkedList<>();
+		Queue<Integer> q = new LinkedList<>();
+		int[] dist = new int[n + 1];
+		Arrays.fill(dist, -1);
+		String[] tokens = br.readLine().split(" ");
+		for (int i = 0; i < m; ++i) {
+			int num = Integer.parseInt(tokens[i]);
+			dist[num] = 0;
+			q.offer(num);
+		}
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < m; i++) {
-            int start = Integer.parseInt(st.nextToken());
-            dist[start] = 0;
-            q.add(start);
-        }
+		int maxBitSize = 32 - Integer.numberOfLeadingZeros(n);
+		while (!q.isEmpty()) {
+			int num = q.poll();
 
-        while (!q.isEmpty()) {
-            var cur = q.poll();
+			for (int k = 0; k < maxBitSize; ++k) {
 
-            for (int k = 0; k < 31; k++) {
-                int nxt;
-                if ((cur & (1 << k)) != 0) {
-                    nxt = cur & ~(1 << k);
-                } else {
-                    nxt = cur | (1 << k);
-                }
+				int nxt;
+				if ((num & (1 << k)) != 0) {
+					nxt = num & ~(1 << k);
+				}
+				else {
+					nxt = num | (1 << k);
+				}
 
-                if (nxt > n || dist[nxt] >= 0) continue;
+				if (nxt > n || dist[nxt] >= 0) continue;
 
-                dist[nxt] = dist[cur] + 1;
-                q.add(nxt);
-            }
-        }
+				dist[nxt] = dist[num] + 1;
+				q.add(nxt);
+			}
+		}
+		int ans = 0;
+		for (int i = 0; i <= n; i++) {
+			ans = Math.max(ans, dist[i]);
+		}
 
-        int ans = 0;
-        for (int i = 0; i <= n; i++) {
-            ans = Math.max(ans, dist[i]);
-        }
-
-        System.out.println(ans);
-    }
+		System.out.println(ans);
+	}
 }
