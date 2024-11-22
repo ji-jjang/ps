2f74caa16520d854f75afd2707606fe147fdbb64
ji-jjang <153375785+ji-jjang@users.noreply.github.com>
2024-11-22 11:31:33 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 448 ms, Memory: 68992 KB -BaekjoonHub

--- 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
@@ -2,24 +2,29 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-				int n = Integer.parseInt(br.readLine());
+	public static void main(String[] args) throws IOException {
 
-				StringBuilder sb = new StringBuilder();
-				sb.append((int)Math.pow(2, n) - 1 + "\n");
-				hanoi(n, 1, 3, sb);
-				System.out.println(sb.toString());
-    }
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		static void hanoi(int n, int a, int c, StringBuilder sb) {
-			if (n == 1) {
-				sb.append(a + " " + c + "\n");
-				return;
-			}
-			hanoi(n - 1, a, 6 - a - c, sb);
+		int n = Integer.parseInt(br.readLine());
+
+		StringBuilder sb = new StringBuilder();
+		sb.append((int)(Math.pow(2, n)) - 1 + "\n");
+		recur(n, 1, 3, sb);
+
+		System.out.println(sb);
+	}
+
+	static void recur(int n, int a, int c, StringBuilder sb) {
+
+		if (n == 1) {
 			sb.append(a + " " + c + "\n");
-			hanoi(n - 1, 6 - a - c, c, sb);
+			return;
 		}
+		int mid = 6 - a - c;
+		recur(n - 1, a, mid, sb);
+		sb.append(a + " " + c + "\n");
+		recur(n - 1, mid, c, sb);
+	}
 }
