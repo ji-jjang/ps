c1ecde212cbd183cb0b38bf70df05bacee78d982
ji-jjang <153375785+ji-jjang@users.noreply.github.com>
2024-11-22 11:21:56 +0900

    [Silver I] Title: 곱셈, Time: 100 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.java
+++ 백준/Silver/1629. 곱셈/곱셈.java
@@ -2,24 +2,27 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-				long a = Long.parseLong(st.nextToken());
-				long b = Long.parseLong(st.nextToken());
-				long c = Long.parseLong(st.nextToken());
+	public static void main(String[] args) throws IOException {
 
-				System.out.println(recur(a, b, c));
-    }
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		static long recur(long a, long b, long c) {
-			if (b == 1)
-				return a % c;
-			long cur = recur(a, b / 2, c);
-			cur = cur * cur % c;
-			if (b % 2 == 0)
-				return cur;
-			return a * cur % c;
-		}
+		String[] tokens = br.readLine().split(" ");
+		long a = Integer.parseInt(tokens[0]);
+		long b = Integer.parseInt(tokens[1]);
+		long c = Integer.parseInt(tokens[2]);
+
+		System.out.println(recur(a, b, c));
+	}
+
+	static long recur(long a, long b, long c) {
+
+		if (b == 1)
+			return a % c;
+		long cur = recur(a, b / 2, c);
+		cur = cur * cur % c;
+		if (b % 2 == 0)
+			return cur;
+		return cur * a % c;
+	}
 }
