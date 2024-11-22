4afaf753d7b39811c76139f9760b79b8b17e315e
ji-jjang <153375785+ji-jjang@users.noreply.github.com>
2024-11-22 12:27:03 +0900

    [Gold V] Title: Z, Time: 100 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Gold/1074. Z/Z.java
+++ 백준/Gold/1074. Z/Z.java
@@ -2,25 +2,40 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-				StringTokenizer st = new StringTokenizer(br.readLine());
+	public static void main(String[] args) throws IOException {
 
-				int n = Integer.parseInt(st.nextToken());
-				int r = Integer.parseInt(st.nextToken());
-				int c = Integer.parseInt(st.nextToken());
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-				System.out.println(recur(n, r, c));
-    }
+		String[] tokens = br.readLine().split(" ");
 
-		static int recur(int n, int r, int c) {
-			if (n == 0) return 0;
+		int n = Integer.parseInt(tokens[0]);
+		int r = Integer.parseInt(tokens[1]);
+		int c = Integer.parseInt(tokens[2]);
 
-			int half = 1 << (n - 1);
-			if (r < half && c < half) return recur(n - 1, r, c);
-			else if (r < half && c >= half) return half * half + recur(n - 1, r, c - half);
-			else if (r >= half && c < half) return 2 * half * half + recur(n - 1, r - half, c);
-			return 3 * half * half + recur(n - 1, r - half, c - half);
+		System.out.println(recur(n, r, c));
+	}
+
+	static int recur(int n, int r, int c) {
+
+		if (n == 0) {
+			return 0;
 		}
+		int half = 1 << (n - 1);
+
+		if (r < half && c < half) {
+
+			return recur(n - 1, r, c); 
+		}
+		else if (r < half && c >= half) {
+
+			return half * half + recur(n - 1, r, c - half);
+		}
+		else if (r >= half && c < half) {
+
+			return 2 * half * half + recur(n - 1, r - half, c);
+		}
+
+		return 3 * half * half + recur(n - 1, r - half, c - half);
+	}
 }
