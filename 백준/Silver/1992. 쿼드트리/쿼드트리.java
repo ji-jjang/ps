9f0e5ea64906d0d19f201acf81bf5b9b115e2c6d
ji-jjang <153375785+ji-jjang@users.noreply.github.com>
2024-11-23 13:26:17 +0900

    [Silver I] Title: 쿼드트리, Time: 108 ms, Memory: 14140 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.java
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.java
@@ -1,50 +1,49 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
+
+	static char[][] board;
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
 		int n = Integer.parseInt(br.readLine());
-
-		int[][] board = new int[n][n];
-
+		board = new char[n][n];
 		for (int i = 0; i < n; ++i) {
-			String line = br.readLine();
+			char[] chars = br.readLine().toCharArray();
 			for (int j = 0; j < n; ++j) {
-				board[i][j] = line.charAt(j) - '0';
+				board[i][j] = chars[j];
 			}
 		}
+
 		StringBuilder sb = new StringBuilder();
-		recur(n, 0, 0, sb, board);
+		recur(n, 0, 0, sb);
 		System.out.println(sb);
 	}
 
-	static void recur(int n, int r, int c, StringBuilder sb, int[][] board) {
+	static void recur(int n, int r, int c, StringBuilder sb) {
 
-		if (checkSame(n, r, c, sb, board)) {
-			sb.append(board[r][c]);
+		char color = board[r][c];
+
+		if (isOneColor(n, r, c, color)) {
+			sb.append(color);
 			return;
 		}
-
 		int nn = n / 2;
+		sb.append("(");
 		for (int i = 0; i < 2; ++i) {
 			for (int j = 0; j < 2; ++j) {
-				recur(nn, r + i * nn, c + j * nn, sb, board);
+				recur(nn, r + i * nn, c + j * nn, sb);
 			}
 		}
 		sb.append(")");
 	}
+	static boolean isOneColor(int n, int r, int c, char color) {
 
-	static boolean checkSame(int n, int r, int c, StringBuilder sb,  int[][] board) {
-
-		int st = board[r][c];
-		for (int i = r; i < r + n; ++i) {
+		for (int i = r; i < r + n; ++ i) {
 			for (int j = c; j < c + n; ++j) {
-				if (board[i][j] != st) {
-					sb.append("(");
-					return false;
-				}
+				if (board[i][j] != color) return false;
 			}
 		}
 		return true;
