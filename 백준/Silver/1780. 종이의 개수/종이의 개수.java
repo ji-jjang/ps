53c8cbdab3eac76bbcfc65041e6a5d1b7191ecb5
ji-jjang <153375785+ji-jjang@users.noreply.github.com>
2024-11-23 12:04:32 +0900

    [Silver II] Title: 종이의 개수, Time: 856 ms, Memory: 319536 KB -BaekjoonHub

--- 백준/Silver/1780. 종이의 개수/종이의 개수.java
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.java
@@ -1,48 +1,47 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
+
+	static int[] ans = new int[4];
+	static int[][] board;
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
 		int n = Integer.parseInt(br.readLine());
-
-		int[][] board = new int[n][n];
-
+		board = new int[n][n];
 		for (int i = 0; i < n; ++i) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
+			String[] tokens = br.readLine().split(" ");
 			for (int j = 0; j < n; ++j) {
-				board[i][j] = Integer.parseInt(st.nextToken());
+				board[i][j] = Integer.parseInt(tokens[j]);
 			}
 		}
-		int[] ans = new int[3];
-		recur(n, 0, 0, ans, board);
-		for (int i = 0; i < 3; ++i) 
-			System.out.println(ans[i]);
+
+		recur(n, 0, 0);
+		System.out.println(ans[0] + "\n" + ans[1] + "\n" + ans[2]);
 	}
 
-	static void recur(int n, int r, int c, int[] ans, int[][] board) {
+	static void recur(int n, int r, int c) {
 
-		if (checkSame(n, r, c, board)) {
-			ans[board[r][c] + 1]++;
+		int color = board[r][c];
+
+		if (isOneColor(n, r, c, color)) {
+			ans[color + 1]++;
 			return;
 		}
-
 		int nn = n / 3;
 		for (int i = 0; i < 3; ++i) {
 			for (int j = 0; j < 3; ++j) {
-				recur(nn, r + i * nn, c + j * nn, ans, board);
+				recur(nn, r + i * nn, c + j * nn);
 			}
 		}
 	}
+	static boolean isOneColor(int n, int r, int c, int color) {
 
-	static boolean checkSame(int n, int r, int c, int[][] board) {
-
-		int st = board[r][c];
-		for (int i = r; i < r + n; ++i) {
+		for (int i = r; i < r + n; ++ i) {
 			for (int j = c; j < c + n; ++j) {
-				if (board[i][j] != st) 
-					return false;
+				if (board[i][j] != color) return false;
 			}
 		}
 		return true;
