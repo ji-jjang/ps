b598390c5f7b19d4c95a235bfe43f4e14218e19e
ji-jjang <153375785+ji-jjang@users.noreply.github.com>
2024-11-25 11:51:19 +0900

    [Gold IV] Title: 별 찍기 - 11, Time: 456 ms, Memory: 145456 KB -BaekjoonHub

--- 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.java
+++ 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.java
@@ -1,56 +1,49 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-	public static void printStar(int r, int c, char[][] board) {
-
-		board[r][c] = '*';
-		board[r + 1][c - 1] = '*';
-		board[r + 1][c + 1] = '*';
-		for (int idx = 0; idx < 5; ++idx) {
-			board[r + 2][c - 2 + idx] = '*';
-		}
-	}
-
-	public static void recur(int r, int c, int h, char[][] board) {
-
-		if (h == 3) {
-			printStar(r, c, board);
-			return;
-		}
-		int hh = h >> 1;
-
-		recur(r, c, hh, board);
-		recur(r + hh, c - hh, hh, board);
-		recur(r + hh, c + hh, hh, board);
-	}
+	static int n;
+	static char[][] board;
 
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int n = Integer.parseInt(br.readLine());
-		char[][] board = new char[n][2 * n - 1];
-
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < 2 * n - 1; j++) {
-				board[i][j] = ' ';
-			}
+		n = Integer.parseInt(br.readLine());
+		board = new char[n][n * 2];
+		for (int i = 0; i < n; ++i) {
+			Arrays.fill(board[i], ' ');
 		}
 
-		recur(0, n - 1, n, board);
+		recur(n, 0, n - 1);
 
 		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < 2 * n - 1; j++) {
+
+		for (int i = 0; i < n; ++i) {
+			for (int j = 0; j < n * 2; ++j) {
 				sb.append(board[i][j]);
 			}
-			sb.append('\n');
+			sb.append("\n");
 		}
-
 		System.out.println(sb);
 	}
-}
 
+	static void recur(int n, int r, int c) {
+		if (n == 3) {
+			board[r][c] = '*';
+			board[r + 1][c - 1] = '*';
+			board[r + 1][c + 1] = '*';
+			for (int i = 0; i < 5; ++i) {
+				board[r + 2][c - 2 + i] = '*';
+			}
+			return;
+		}
+
+		int nn = n / 2;
+
+		recur(nn, r, c);
+		recur(nn, r + nn, c - nn);
+		recur(nn, r + nn, c + nn);
+	}
+}
