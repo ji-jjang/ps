e7a378e6d18c1b022e6e861ee60c9466c8e92b56
ji-jjang <153375785+ji-jjang@users.noreply.github.com>
2024-11-29 12:05:35 +0900

    [Silver II] Title: N과 M (9), Time: 392 ms, Memory: 48020 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.java
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -3,52 +3,50 @@ import java.util.*;
 
 public class Main {
 
+	static int n, m;
+	static int[] nums;
+	static Set<String> set = new LinkedHashSet<>();
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
-		int[] nums = new int[n];
-		int[] seq = new int[m + 1];
-		boolean[] isSelected = new boolean[n];
+		String[] tokens = br.readLine().split(" ");
 
-		st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(tokens[0]);
+		m = Integer.parseInt(tokens[1]);
+		nums = new int[n];
+
+		tokens = br.readLine().split(" ");
 		for (int i = 0; i < n; ++i) {
-			nums[i] = Integer.parseInt(st.nextToken());
+			nums[i] = Integer.parseInt(tokens[i]);
 		}
 		Arrays.sort(nums);
 
+		dfs(0, new int[n + 1], new boolean[n]);
 		StringBuilder sb = new StringBuilder();
-		Set<String> hs = new LinkedHashSet<>(); 
-		dfs(0, n, m, nums, seq, sb, isSelected, hs);
-		Iterator<String> it = hs.iterator();
-		sb.setLength(0);
-		while (it.hasNext()) {
-			sb.append(it.next());
-			sb.append("\n");
+		for (var e : set) {
+			sb.append(e + "\n");
 		}
 		System.out.println(sb);
 	}
 
-	public static void dfs(int depth, int n, int m, int[] nums, int[] seq, StringBuilder sb, boolean[] isSelected, Set<String> hs) {
+	static void dfs(int depth, int[] selected, boolean[] isVisited) {
 
 		if (depth == m) {
+			StringBuilder sb = new StringBuilder();
 			for (int i = 0; i < m; ++i) {
-				sb.append(seq[i] + " ");
+				sb.append(selected[i] + " ");
 			}
-			hs.add(sb.toString());
-			sb.setLength(0);
+			set.add(sb.toString());
 			return;
 		}
 
 		for (int i = 0; i < n; ++i) {
-			if (isSelected[i]) continue;
-			isSelected[i] = true;
-			seq[depth] = nums[i];
-			dfs(depth + 1, n, m, nums, seq, sb, isSelected, hs);
-			isSelected[i] = false;
+			if (isVisited[i]) continue;
+			isVisited[i] = true;
+			selected[depth] = nums[i];
+			dfs(depth + 1, selected, isVisited);
+			isVisited[i] = false;
 		}
 	}
 }
