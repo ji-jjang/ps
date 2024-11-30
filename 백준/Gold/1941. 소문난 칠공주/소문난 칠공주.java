80e65bdc9265757e0b294cfadc4108a0b65d3164
ji-jjang <153375785+ji-jjang@users.noreply.github.com>
2024-11-30 14:40:12 +0900

    [Gold III] Title: 소문난 칠공주, Time: 664 ms, Memory: 223076 KB -BaekjoonHub

--- 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.java
+++ 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.java
@@ -1,98 +1,83 @@
-import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int[] dy = {-1, 1, 0, 0}; // 상하좌우
-    static int[] dx = {0, 0, -1, 1};
-    static char[][] seats = new char[5][5];
-    static boolean[][] isSelected = new boolean[5][5];
-    static int ans = 0;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        for (int i = 0; i < 5; ++i) {
-            String line = br.readLine();
-            for (int j = 0; j < 5; ++j) {
-                seats[i][j] = line.charAt(j);
-            }
-        }
+	static char[][] grid = new char[5][5];
+	static boolean[][] isVisited = new boolean[5][5];
+	static int[] dy = {1, -1, 0, 0};
+	static int[] dx = {0, 0, 1, -1};
+	static int ans = 0;
 
-        List<int[]> pos = new ArrayList<>();
-        for (int i = 0; i < 5; ++i) {
-            for (int j = 0; j < 5; ++j) {
-                pos.add(new int[]{i, j});
-            }
-        }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		for (int i = 0; i < 5; i++) {
+			grid[i] = sc.next().toCharArray();
+		}
 
-        dfs(pos, new ArrayList<>(), 0, 0);
+		List<int[]> points = new ArrayList<>();
+		for (int i = 0; i < 5; ++i) {
+			for (int j = 0; j < 5; ++j) {
+				points.add(new int[]{i, j});
+			}
+		}
+		dfs(0, 0, points, new ArrayList<>());
+		System.out.println(ans);
+	}
 
-        System.out.println(ans);
-    }
+	static void dfs(int depth, int st, List<int[]> points, List<int[]> selected) {
 
-    public static void dfs(List<int[]> pos, List<int[]> sseats, int idx, int daCnt) {
+		if (depth == 7) {
+			if (isValid(selected)) ++ans;
+			return;
+		}
 
-        if (sseats.size() == 7) {
-            if (daCnt >= 4) {
-                if (isAdjacent(sseats)) {
-                    ++ans;
-                }
-            }
-            return;
-        }
+		for (int i = st; i < points.size(); i++) {
+			selected.add(points.get(i));
+			dfs(depth + 1, i + 1, points, selected);
+			selected.remove(selected.size() - 1);
+		}
+	}
 
-        if (idx >= pos.size()) {
-            return;
-        }
+	static boolean isValid(List<int[]> selected) {
 
-        int[] cur = pos.get(idx);
-        int y = cur[0];
-        int x = cur[1];
-        char student = seats[y][x];
+		int sCnt = 0;
+		for (var e : selected) {
+			if (grid[e[0]][e[1]] == 'S') ++sCnt;
+		}
+		if (sCnt < 4) return false;
 
-        sseats.add(cur);
-        dfs(pos, sseats, idx + 1, daCnt + (student == 'S' ? 1 : 0));
-        sseats.remove(sseats.size() - 1);
+		return isConnected(selected);
+	}
 
-        dfs(pos, sseats, idx + 1, daCnt);
-    }
+	static boolean isConnected(List<int[]> selected) {
 
-    public static boolean isAdjacent(List<int[]> sseats) {
-        boolean[][] isVisited = new boolean[5][5];
-        Queue<int[]> q = new LinkedList<>();
-        int cnt = 1;
+		boolean[][] isChecked = new boolean[5][5];
+		for (var pos : selected) {
+			isChecked[pos[0]][pos[1]] = true;
+		}
 
-        int y = sseats.get(0)[0];
-        int x = sseats.get(0)[1];
+		Queue<int[]> q = new LinkedList<>();
+		q.add(selected.get(0));
+		boolean[][] isChecked2 = new boolean[5][5];
+		isChecked2[selected.get(0)[0]][selected.get(0)[1]] = true;
 
-        q.offer(new int[]{y, x});
-        isVisited[y][x] = true;
+		int cnt = 0;
+		while (!q.isEmpty()) {
+			var cur = q.poll();
+			++cnt;
 
-        Set<String> selectedSet = new HashSet<>();
-        for (int[] pos : sseats) {
-            selectedSet.add(pos[0] + "," + pos[1]);
-        }
+			for (int dir = 0; dir < 4; ++dir) {
+				int ny = cur[0] + dy[dir];
+				int nx = cur[1] + dx[dir];
+				if (ny < 0 || ny >= 5 || nx < 0 || nx >= 5) continue;
+				if (isChecked2[ny][nx]) continue;
+				if (!isChecked[ny][nx]) continue;
+				isChecked2[ny][nx] = true;
+				q.add(new int[]{ny, nx});
+			}
+		}
 
-        while (!q.isEmpty()) {
-            int[] cur = q.poll();
-            y = cur[0];
-            x = cur[1];
-
-            for (int dir = 0; dir < 4; ++dir) {
-                int ny = y + dy[dir];
-                int nx = x + dx[dir];
-
-                if (ny >= 0 && ny < 5 && nx >= 0 && nx < 5) {
-                    if (!isVisited[ny][nx] && selectedSet.contains(ny + "," + nx)) {
-                        isVisited[ny][nx] = true;
-                        q.offer(new int[]{ny, nx});
-                        ++cnt;
-                    }
-                }
-            }
-        }
-
-        return cnt == 7;
-    }
+		return cnt == 7;
+	}
 }
 
