a285f4eb37f60a9d1e4931dbf26975d7263aebc0
ji-jjang <153375785+ji-jjang@users.noreply.github.com>
2024-12-03 13:47:35 +0900

    [Gold I] Title: Gaaaaaaaaaarden, Time: 1756 ms, Memory: 308936 KB -BaekjoonHub

--- 백준/Gold/18809. Gaaaaaaaaaarden/Gaaaaaaaaaarden.java
+++ 백준/Gold/18809. Gaaaaaaaaaarden/Gaaaaaaaaaarden.java
@@ -1,131 +1,147 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
 
+	static int r, c;
+	static int gr, re;
+	static int[][] garden;
+	static List<int[]> pos = new ArrayList<>();
 	static int[] dy = {-1, 0, 1, 0};
 	static int[] dx = {0, 1, 0, -1};
 	static int ans = 0;
-	static int[][] board;
 
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int r = Integer.parseInt(st.nextToken());
-		int c = Integer.parseInt(st.nextToken());
-		int green = Integer.parseInt(st.nextToken());
-		int red = Integer.parseInt(st.nextToken());
+		String[] tokens = br.readLine().split(" ");
+		r = Integer.parseInt(tokens[0]);
+		c = Integer.parseInt(tokens[1]);
+		gr = Integer.parseInt(tokens[2]);
+		re = Integer.parseInt(tokens[3]);
+		garden = new int[r][c];
 
-		board = new int[r][c];
-		List<int[]> pos = new ArrayList<>();
 
 		for (int i = 0; i < r; ++i) {
-			st = new StringTokenizer(br.readLine());
+			tokens = br.readLine().split(" ");
 			for (int j = 0; j < c; ++j) {
-				board[i][j] = Integer.parseInt(st.nextToken());
-				if (board[i][j] == 2) {
+				garden[i][j] = Integer.parseInt(tokens[j]);
+				if (garden[i][j] == 2) {
 					pos.add(new int[]{i, j});
 				}
 			}
 		}
-		int[] color = new int[pos.size()];
-		dfs(0, r, c, green, red, color, pos);
+		dfs(0, new ArrayList<>(), new ArrayList<>());
 		System.out.println(ans);
 	}
 
-	public static void dfs(int depth, int r, int c, int green, int red, int[] color, List<int[]> pos) {
+	static void dfs(int depth, List<int[]> green, List<int[]> red) {
 
-		if (green + red == 0) {
-			bfs(r, c, color, pos);
+		if (green.size() == gr && red.size() == re) {
+			ans = Math.max(ans, bfs(green, red));
 			return;
 		}
 
-		if (depth == pos.size())
+		if (depth >= pos.size()) {
 			return;
-
-		if (green > 0) {
-			color[depth] = 2;
-			dfs(depth + 1, r, c, green - 1, red, color, pos);
-			color[depth] = 0;
 		}
 
-		if (red > 0) {
-			color[depth] = 1;
-			dfs(depth + 1, r, c, green, red - 1, color, pos);
-			color[depth] = 0;
+		var cur = pos.get(depth);
+
+		if (green.size() < gr) {
+			green.add(cur);
+			dfs(depth + 1, green, red);
+			green.remove(green.size() - 1);
 		}
-		dfs(depth + 1, r, c, green, red, color, pos);
+		if (red.size() < re) {
+			red.add(cur);
+			dfs(depth + 1, green, red);
+			red.remove(red.size() - 1);
+		}
+		dfs(depth + 1, green, red);
 	}
 
-	public static void bfs(int r, int c, int[] color, List<int[]> pos) {
+	static int bfs(List<int[]> green, List<int[]> red) {
 
-		Queue<int[]> q = new LinkedList<>(); // y, x, time, color
-		int[][] greenMap = new int[r][c];
-		int[][] redMap = new int[r][c];
+		int[][] dist1 = new int[r][c];
+		int[][] dist2 = new int[r][c];
 		boolean[][] flower = new boolean[r][c];
 
-		for (int[] row : greenMap) Arrays.fill(row, -1);
-		for (int[] row : redMap) Arrays.fill(row, -1);
+		for (int i = 0; i < r; ++i) {
+			Arrays.fill(dist1[i], -1);
+			Arrays.fill(dist2[i], -1);
+		}
+		Queue<int[]> q1 = new LinkedList<>();
+		Queue<int[]> q2 = new LinkedList<>();
 
-		for (int i = 0; i < color.length; ++i) {
-			int y = pos.get(i)[0];
-			int x = pos.get(i)[1];
-			if (color[i] == 2) {
-				greenMap[y][x] = 0;
-				q.add(new int[]{y, x, 0, 2}); 
-			} else if (color[i] == 1) {
-				redMap[y][x] = 0;
-				q.add(new int[]{y, x, 0, 1});
-			}
+		for (var e : green) {
+			dist1[e[0]][e[1]] = 0;
+			q1.add(e);
+		}
+
+		for (var e : red) {
+			dist2[e[0]][e[1]] = 0;
+			q2.add(e);
 		}
 
 		int cnt = 0;
-		while (!q.isEmpty()) {
-			int[] cur = q.poll();
-			int y = cur[0];
-			int x = cur[1];
-			int time = cur[2];
-			int type = cur[3];
+		while (!q1.isEmpty() || !q2.isEmpty()) {
 
-			if (flower[y][x]) continue;
+			int size = q1.size();
+			for (int i = 0; i < size; ++i) {
 
-			if (greenMap[y][x] == redMap[y][x] && greenMap[y][x] != -1) {
-				flower[y][x] = true;
-				++cnt;
-				continue;
+				var cur = q1.poll();
+				int y = cur[0];
+				int x = cur[1];
+
+				if (flower[y][x]) continue;
+
+				for (int dir = 0; dir < 4; ++dir) {
+					int ny = y + dy[dir];
+					int nx = x + dx[dir];
+
+					if (ny < 0 || ny >= r || nx < 0 || nx >= c) continue;
+
+					if (dist1[ny][nx] != -1 || garden[ny][nx] == 0) continue;
+
+					if (!flower[ny][nx] && dist2[ny][nx] != -1 && dist2[ny][nx] == dist1[y][x] + 1) {
+						flower[ny][nx] = true;
+						++cnt;
+						continue;
+					}
+
+					dist1[ny][nx] = dist1[y][x] + 1;
+					q1.add(new int[]{ny, nx});
+				}
 			}
-			for (int dir = 0; dir < 4; ++dir) {
-				int ny = y + dy[dir];
-				int nx = x + dx[dir];
+			size = q2.size();
+			for (int i = 0; i < size; ++i) {
+				var cur = q2.poll();
+				int y = cur[0];
+				int x = cur[1];
 
-				if (ny < 0 || ny >= r || nx < 0 || nx >= c) continue;
-				if (board[ny][nx] == 0 || flower[ny][nx]) continue;
+				if (flower[y][x]) 
+					continue;
 
-				if (type == 2) {
-					if (greenMap[ny][nx] == -1) {
-						greenMap[ny][nx] = time + 1;
-						if (redMap[ny][nx] != -1 && redMap[ny][nx] == time + 1) {
-							flower[ny][nx] = true;
-							cnt++;
-						} else {
-							q.add(new int[]{ny, nx, time + 1, 2});
-						}
+				for (int dir = 0; dir < 4; ++dir) {
+
+					int ny = y + dy[dir];
+					int nx = x + dx[dir];
+
+					if (ny < 0 || ny >= r || nx < 0 || nx >= c) continue;
+					if (garden[ny][nx] == 0 || dist2[ny][nx] != -1) continue;
+					if (!flower[ny][nx] && dist1[ny][nx] != -1 && dist1[ny][nx] == dist2[y][x] + 1) {
+						flower[ny][nx] = true;
+						++cnt;
+						continue;
 					}
-				} else if (type == 1) {
-					if (redMap[ny][nx] == -1) {
-						redMap[ny][nx] = time + 1;
-						if (greenMap[ny][nx] != -1 && greenMap[ny][nx] == time + 1) {
-							flower[ny][nx] = true;
-							cnt++;
-						} else {
-							q.add(new int[]{ny, nx, time + 1, 1});
-						}
-					}
+
+					dist2[ny][nx] = dist2[y][x] + 1;
+					q2.add(new int[]{ny, nx});
 				}
 			}
 		}
-		ans = Math.max(ans, cnt);
+		return cnt;
 	}
 }
