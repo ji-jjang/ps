e23de566c10db4a6f4c00a077b885418ceffb3c9
ji-jjang <153375785+ji-jjang@users.noreply.github.com>
2024-12-04 16:33:36 +0900

    [Gold I] Title: 비숍, Time: 148 ms, Memory: 15632 KB -BaekjoonHub

--- 백준/Gold/1799. 비숍/비숍.java
+++ 백준/Gold/1799. 비숍/비숍.java
@@ -1,56 +1,57 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
 
+	static int n;
+	static int[][] board;
+	static List<int[]> pos1 = new ArrayList<>();
+	static List<int[]> pos2 = new ArrayList<>();
+	static boolean[] diag1 = new boolean[50];
+	static boolean[] diag2 = new boolean[50];
+	static int[] ans = new int[2];
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int n = Integer.parseInt(br.readLine());
-		int[][] board = new int[n][n];
+		n = Integer.parseInt(br.readLine());
+		board = new int[n][n];
 
-		List<int[]> black = new ArrayList<>();
-		List<int[]> white = new ArrayList<>();
 		for (int i = 0; i < n; ++i) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
+			String[] tokens = br.readLine().split(" ");
 			for (int j = 0; j < n; ++j) {
-				board[i][j] = Integer.parseInt(st.nextToken());
+				board[i][j] = Integer.parseInt(tokens[j]);
 				if (board[i][j] == 1) {
-					if ((i + j) % 2 == 0) black.add(new int[]{i, j});
-					else white.add(new int[]{i, j});
+					if ((i + j) % 2 == 0) {
+						pos1.add(new int[]{i, j});
+					} else {
+						pos2.add(new int[]{i, j});
+					}
 				}
 			}
 		}
-
-		boolean[] diag1 = new boolean[50];
-		boolean[] diag2 = new boolean[50];
-
-		int ansBlack = dfs(0, 0, n, diag1, diag2, board, black);
-		int ansWhite = dfs(0, 0, n, diag1, diag2, board, white);
-		System.out.println(ansBlack + ansWhite);
+		dfs(0, 0, pos1, 0);
+		dfs(0, 0, pos2, 1);
+		System.out.println(ans[0] + ans[1]);
 	}
 
-	public static int dfs(int depth, int cnt, int n, boolean[] diag1, boolean[] diag2, int[][] board, List<int[]> pos) {
+	static void dfs(int depth, int count, List<int[]> pos, int idx) {
 
-		if (depth == pos.size()) return cnt;
-
+		if (depth == pos.size()) {
+			ans[idx] = Math.max(ans[idx], count);
+			return;
+		}
 		var cur = pos.get(depth);
 		int row = cur[0];
 		int col = cur[1];
 
-		int maxCnt = cnt;
-		if (!diag1[row + col] && !diag2[row - col + n - 1]) {
+		if (!diag1[row + col] && !diag2[row - col + n]) {
 			diag1[row + col] = true;
-			diag2[row - col + n - 1] = true;
-
-			maxCnt = Math.max(maxCnt, dfs(depth + 1, cnt + 1, n, diag1, diag2, board, pos));
-
+			diag2[row - col + n] = true;
+			dfs(depth + 1, count + 1, pos, idx);
 			diag1[row + col] = false;
-			diag2[row - col + n - 1] = false;
+			diag2[row - col + n] = false;
 		}
-		maxCnt = Math.max(maxCnt, dfs(depth + 1, cnt, n, diag1, diag2, board, pos));
-
-		return maxCnt;
+		dfs(depth + 1, count, pos, idx);
 	}
 }
