4fdc5f8f6b3ae38e7a9719ae191b368cb5175a55
ji-jjang <153375785+ji-jjang@users.noreply.github.com>
2024-12-17 22:39:01 +0900

    [Gold V] Title: 치킨 배달, Time: 260 ms, Memory: 19192 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -1,58 +1,66 @@
-import java.io.*;
 import java.util.*;
-
+import java.io.*;
 
 public class Main {
 
-	static int[][] map;
-	static List<int[]> houses = new ArrayList<>();
-	static List<int[]> chickens = new ArrayList<>();
-	static int[] isSelected;
 	static int n, m;
-	static int ans = 0x7fffffff;
+	static int[][] city;
+	static List<int[]> chickens = new ArrayList<>();
+	static List<int[]> houses = new ArrayList<>();
+	static int ans = Integer.MAX_VALUE;
+
 	public static void main(String[] args) throws IOException {
-		
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
+		String[] tokens = br.readLine().split(" ");
 
-		map = new int[n][n];
-		isSelected = new int[m];
+		n = Integer.parseInt(tokens[0]);
+		m = Integer.parseInt(tokens[1]);
+
+		city = new int[n][n];
 		for (int i = 0; i < n; ++i) {
-			st = new StringTokenizer(br.readLine());
+			tokens = br.readLine().split(" ");
 			for (int j = 0; j < n; ++j) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-				if (map[i][j] == 1) houses.add(new int[]{i, j});
-				else if (map[i][j] == 2) chickens.add(new int[]{i, j});
+				city[i][j] = Integer.parseInt(tokens[j]);
+				if (city[i][j] == 1) {
+					houses.add(new int[]{i, j});
+				}
+				else if (city[i][j] == 2) {
+					chickens.add(new int[]{i, j});
+				}
 			}
 		}
-		dfs(0, 0);
+		dfs(0, 0, new ArrayList<>(), new boolean[chickens.size()]);
 		System.out.println(ans);
 	}
 
-	public static void dfs(int depth, int st) {
+	static void dfs(int depth, int st, List<Integer> selected, boolean[] isVisited) {
 
 		if (depth == m) {
-			int perChickDist = 0;
+
+			int totalDist = 0;
 			for (var h : houses) {
-				int hy = h[0];
-				int hx = h[1];
-				int minChickDist = 0x7fffffff;
+
+				int chickenDist = Integer.MAX_VALUE;
 				for (int i = 0; i < m; ++i) {
-					int cy = chickens.get(isSelected[i])[0];
-					int cx = chickens.get(isSelected[i])[1];
-					minChickDist = Math.min(minChickDist, Math.abs(hy - cy) + Math.abs(hx - cx));
+					int idx = selected.get(i);
+					chickenDist = Math.min(chickenDist, Math.abs(h[0] - chickens.get(idx)[0]) + Math.abs(h[1] - chickens.get(idx)[1]));
 				}
-				perChickDist += minChickDist;
+				totalDist += chickenDist;
 			}
-			ans = Math.min(ans, perChickDist);
-			return;
+			ans = Math.min(ans, totalDist);
+			return; 
 		}
+
 		for (int i = st; i < chickens.size(); ++i) {
-			isSelected[depth] = i;
-			dfs(depth + 1, i + 1);
+
+			if (isVisited[i]) continue;
+			isVisited[i] = true;
+			selected.add(i);
+			dfs(depth + 1, i, selected, isVisited);
+			selected.remove(selected.size() - 1);
+			isVisited[i] = false;
 		}
 	}
 }
