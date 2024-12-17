d49bf5064fce3a6985ae5b9efd8bfc99ca22bd07
ji-jjang <153375785+ji-jjang@users.noreply.github.com>
2024-12-17 12:55:46 +0900

    [Gold I] Title: 2048 (Easy), Time: 458 ms, Memory: 52523 KB -BaekjoonHub

--- 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.java
+++ 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.java
@@ -1,12 +1,12 @@
-import java.io.*;
 import java.util.*;
-
+import java.io.*;
 
 public class Main {
 
-	static int[][] board;
 	static int n;
-
+	static int[][] board;
+	static int[][] copy;
+	static int ans = 0;
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,83 +14,81 @@ public class Main {
 		n = Integer.parseInt(br.readLine());
 
 		board = new int[n][n];
-		int[][] origin = new int[n][n];
+		copy = new int[n][n];
 		for (int i = 0; i < n; ++i) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
+			String[] tokens = br.readLine().split(" ");
 			for (int j = 0; j < n; ++j) {
-				origin[i][j] = Integer.parseInt(st.nextToken());
+				board[i][j] = Integer.parseInt(tokens[j]);
 			}
 		}
-
-		int[] sdir = new int[5];
-		int ans = 0;
-		for (int i = 0; i < (1 << (2 * 5)); ++i) {
-
-			for (int j = 0; j < n; ++j) {
-				board[j] = origin[j].clone();
-			}
-
-			int cur = i;
-			for (int j = 0; j < 5; ++j) {
-				sdir[j] = cur % 4;
-				cur /= 4;
-			}
-			for (int k = 0; k < 5; ++k) {
-				compress(sdir[k]);
-			}
-			for (int j = 0; j < n; ++j) {
-				for (int k = 0; k < n; ++k) {
-					ans = Math.max(ans, board[j][k]);
-				}
-			}
-		}
-
+		dfs(0, new ArrayList<>());
 		System.out.println(ans);
 	}
 
-	public static void compress(int cnt) {
-		while (cnt-- != 0) {
-			rotate();
+	static void dfs(int depth, List<Integer> selected) {
+
+		if (depth == 5) {
+			for (int i = 0; i < n; ++i) {
+				for (int j = 0; j < n; ++j) {
+					copy[i][j] = board[i][j];
+				}
+			}
+			for (int i = 0; i < 5; ++i) {
+				rotate(selected.get(i));
+				push();
+			}
+			for (int i = 0; i < n; ++i) {
+				for (int j = 0; j < n; ++j) {
+					ans = Math.max(ans, copy[i][j]);
+				}
+			}
+			return; 
 		}
-		comp();
+		for (int i = 0; i < 4; ++i) {
+			selected.add(i);
+			dfs(depth + 1, selected);
+			selected.remove(selected.size() - 1);
+		}
 	}
 
-	public static void rotate() {
+	static void rotate(int dir) {
 
-		int[][] tmp = new int[n][n];
-
-		for (int i = 0; i < n; ++i) {
-			for (int j = 0; j < n; ++j) {
-				tmp[j][n - i - 1] = board[i][j];
+		while (dir-- > 0) {
+			int[][] tmp = new int[n][n];
+			for (int i = 0; i < n; ++i) {
+				for (int j = 0; j < n; ++j) {
+					tmp[j][n - i - 1] = copy[i][j];
+				}
 			}
-		}
-
-		for (int i = 0; i < n; ++i) {
-			for (int j = 0; j < n; ++j) {
-				board[i][j] = tmp[i][j];
+			for (int i = 0; i < n; ++i) {
+				for (int j = 0; j < n; ++j) {
+					copy[i][j] = tmp[i][j];
+				}
 			}
 		}
 	}
 
-	// push dir <-
-	public static void comp() { 
+	static void push() {
+
 		for (int row = 0; row < n; ++row) {
 			int[] tmp = new int[n];
 			int idx = 0;
 			for (int col = 0; col < n; ++col) {
-				if (board[row][col] == 0)
+				if (copy[row][col] == 0) {
 					continue;
+				} 
 				if (tmp[idx] == 0) {
-					tmp[idx] = board[row][col];
-					continue;
+					tmp[idx] = copy[row][col];
 				}
-				if (board[row][col] == tmp[idx])
+				else if (tmp[idx] == copy[row][col]) {
 					tmp[idx++] *= 2;
-				else if (board[row][col] != tmp[idx])
-					tmp[++idx] = board[row][col];
+				}
+				else if (tmp[idx] != copy[row][col]) {
+					tmp[++idx] = copy[row][col];
+				}
 			}
 			for (int i = 0; i < n; ++i) {
-				board[row][i] = tmp[i];
+				copy[row][i] = tmp[i];
 			}
 		}
 	}
