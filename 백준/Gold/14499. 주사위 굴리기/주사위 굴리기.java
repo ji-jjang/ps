20e47c1e5a4840304883500a80b153b4ca68a3cb
ji-jjang <153375785+ji-jjang@users.noreply.github.com>
2024-12-20 12:11:47 +0900

    [Gold IV] Title: 주사위 굴리기, Time: 132 ms, Memory: 16192 KB -BaekjoonHub

--- 백준/Gold/14499. 주사위 굴리기/주사위 굴리기.java
+++ 백준/Gold/14499. 주사위 굴리기/주사위 굴리기.java
@@ -1,81 +1,104 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-public class Main {
+public class main {
 
 	static int[][] dice = new int[4][3];
+	static int[][] map;
+	static list<integer> dirs = new arraylist<>();
 	static int[] dy = {0, 0, 0, -1, 1};
 	static int[] dx = {0, 1, -1, 0, 0};
+	public static void main(string[] args) throws ioexception {
 
-	public static void main(String[] args) throws IOException {
+		bufferedreader br = new bufferedreader(new inputstreamreader(system.in));
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		string[] tokens = br.readline().split(" ");
 
-		int r = Integer.parseInt(st.nextToken());
-		int c = Integer.parseInt(st.nextToken());
-		int y = Integer.parseInt(st.nextToken());
-		int x = Integer.parseInt(st.nextToken());
-		int k = Integer.parseInt(st.nextToken());
+		int r = integer.parseint(tokens[0]);
+		int c = integer.parseint(tokens[1]);
+		int sy = integer.parseint(tokens[2]);
+		int sx = integer.parseint(tokens[3]);
+		int k = integer.parseint(tokens[4]);
 
-		int[][] map = new int[r][c];
+		map = new int[r][c];
 		for (int i = 0; i < r; ++i) {
-			st = new StringTokenizer(br.readLine());
+			tokens = br.readline().split(" ");
 			for (int j = 0; j < c; ++j) {
-				map[i][j] = Integer.parseInt(st.nextToken());
+				map[i][j] = integer.parseint(tokens[j]);
 			}
 		}
-		st = new StringTokenizer(br.readLine());
-		StringBuilder sb = new StringBuilder();
+		tokens = br.readline().split(" ");
 		for (int i = 0; i < k; ++i) {
-			int dir = Integer.parseInt(st.nextToken());
-			int ny = y + dy[dir];
-			int nx = x + dx[dir];
-			if (ny < 0 || ny >= r || nx < 0 || nx >= c) continue;
-			move(dir);
-			y = ny;
-			x = nx;
-			if (map[y][x] == 0) {
-				map[y][x] = dice[3][1];
-			}
-			else {
-				dice[3][1] = map[y][x];
-				map[y][x] = 0;
+			dirs.add(integer.parseint(tokens[i]));
+		}
+		
+		stringbuilder sb = new stringbuilder();
+		for (var dir : dirs) {
+			if (sy + dy[dir] >= r || sy + dy[dir] < 0 || sx + dx[dir] >= c || sx + dx[dir] < 0) continue;
+			sy += dy[dir];
+			sx += dx[dir];
+			if (dir == 1) rotate_east();
+			else if (dir == 2) rotate_west();
+			else if (dir == 3) rotate_north();
+			else rotate_south();
+
+			if (map[sy][sx] == 0) {
+				map[sy][sx] = dice[3][1];
+			} else {
+				dice[3][1] = map[sy][sx];
+				map[sy][sx] = 0;
 			}
 			sb.append(dice[1][1] + "\n");
 		}
-		System.out.println(sb);
+		system.out.println(sb);
 	}
 
-	public static void move(int dir) {
-
-		int[][] tmp = new int[4][3];
-		for (int i = 0; i < 4; ++i) {
-			for (int j = 0; j < 3; ++j) {
-				tmp[i][j] = dice[i][j];
-			}
+	static void rotate_north() {
+		int[] tmp = new int[4];
+		for (int row = 0; row < 4; ++row) {
+			tmp[row] = dice[row][1];
 		}
-
-		if (dir == 1) {
-			dice[1][0] = tmp[3][1];
-			dice[1][1] = tmp[1][0];
-			dice[1][2] = tmp[1][1];
-			dice[3][1] = tmp[1][2];
-		} else if (dir == 2) {
-			dice[1][0] = tmp[1][1];
-			dice[1][1] = tmp[1][2];
-			dice[1][2] = tmp[3][1];
-			dice[3][1] = tmp[1][0];
-		} else if (dir == 3) {
-			dice[0][1] = tmp[1][1];
-			dice[1][1] = tmp[2][1];
-			dice[2][1] = tmp[3][1];
-			dice[3][1] = tmp[0][1];
-		} else {
-			dice[0][1] = tmp[3][1];
-			dice[1][1] = tmp[0][1];
-			dice[2][1] = tmp[1][1];
-			dice[3][1] = tmp[2][1];
+		for (int i = 1; i < 4; ++i) {
+			dice[i - 1][1] = tmp[i];
 		}
+		dice[3][1] = tmp[0];
+	}
+
+	static void rotate_east() {
+		int[] tmp = new int[4];
+		tmp[0] = dice[1][0];
+		tmp[1] = dice[1][1];
+		tmp[2] = dice[1][2];
+		tmp[3] = dice[3][1];
+
+		dice[1][0] = tmp[3];
+		dice[1][1] = tmp[0];
+		dice[1][2] = tmp[1];
+		dice[3][1] = tmp[2];
+	}
+
+	static void rotate_south() {
+		int[] tmp = new int[4];
+		for (int row = 0; row < 4; ++row) {
+			tmp[row] = dice[row][1];
+		}
+		for (int i = 0; i < 3; ++i) {
+			dice[i + 1][1] = tmp[i];
+		}
+		dice[0][1] = tmp[3];
+	}
+	
+	static void rotate_west() {
+		int[] tmp = new int[4];
+		tmp[0] = dice[1][0];
+		tmp[1] = dice[1][1];
+		tmp[2] = dice[1][2];
+		tmp[3] = dice[3][1];
+
+
+		dice[1][0] = tmp[1];
+		dice[1][1] = tmp[2];
+		dice[1][2] = tmp[3];
+		dice[3][1] = tmp[0];
 	}
 }
