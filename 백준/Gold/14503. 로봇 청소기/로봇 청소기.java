f0ed3fa5298f48d59822e3f498b74ed9fe06247d
ji-jjang <153375785+ji-jjang@users.noreply.github.com>
2024-12-25 14:09:07 +0900

    [Gold V] Title: 로봇 청소기, Time: 108 ms, Memory: 14468 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
@@ -1,37 +1,35 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
 
+	static int r, c;
+	static int sy, sx, sdir;
+	static int[][] room;
 	static int[] dy = {-1, 0, 1, 0};
 	static int[] dx = {0, 1, 0, -1};
-	static int r, c;
-	static int[][] room;
-
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		r = Integer.parseInt(st.nextToken());
-		c = Integer.parseInt(st.nextToken());
+		String[] tokens = br.readLine().split(" ");
+		r = Integer.parseInt(tokens[0]);
+		c = Integer.parseInt(tokens[1]);
 		room = new int[r][c];
-		
-		st = new StringTokenizer(br.readLine());
-		int sy = Integer.parseInt(st.nextToken());
-		int sx = Integer.parseInt(st.nextToken());
-		int sdir = Integer.parseInt(st.nextToken());
+
+		tokens = br.readLine().split(" ");
+		sy = Integer.parseInt(tokens[0]);
+		sx = Integer.parseInt(tokens[1]);
+		sdir = Integer.parseInt(tokens[2]);
 
 		for (int i = 0; i < r; ++i) {
-			st = new StringTokenizer(br.readLine());
+			tokens = br.readLine().split(" ");
 			for (int j = 0; j < c; ++j) {
-				room[i][j] = Integer.parseInt(st.nextToken());
+				room[i][j] = Integer.parseInt(tokens[j]);
 			}
 		}
 
-		Queue<int[]> q = new LinkedList<>();
-
-		q.add(new int[]{sy, sx, sdir});
+		Queue<int[]> q = new LinkedList<>(); // y, x, dir
+		q.offer(new int[]{sy, sx, sdir});
 
 		int ans = 0;
 		while (!q.isEmpty()) {
@@ -39,44 +37,49 @@ public class Main {
 			int y = cur[0];
 			int x = cur[1];
 			int dir = cur[2];
-
 			if (room[y][x] == 0) {
-				room[y][x] = 2;
 				++ans;
+				room[y][x] = 2;
 			}
-			if (!hasDirtyRoom(y, x)) {
+			if (isCleaned(y, x)) {
 				int newDir = (dir + 2) % 4;
 				int ny = y + dy[newDir];
 				int nx = x + dx[newDir];
-				if (ny < 0 || ny >= r || nx < 0 || nx >= c) continue;
-				if (room[ny][nx] == 1) continue;
-				q.add(new int[]{ny, nx, dir});
+				if (ny < 0 || ny >= r || nx < 0 || nx >= c) 
+					continue;
+				if (room[ny][nx] != 1) {
+					q.offer(new int[]{ny, nx, dir});
+				}
 			} else {
-				dir -= 1;
-				if (dir < 0)
-					dir = 3;
-				int ny = y + dy[dir];
-				int nx = x + dx[dir];
-				if (ny < 0 || ny >= r || nx < 0 || nx >= c) continue;
-				if (room[ny][nx] == 0)
-					q.add(new int[]{ny, nx, dir});
-				else
-					q.add(new int[]{y, x, dir});
-
+				for (int i = 0; i < 4; ++i) {
+					dir -= 1;
+					if (dir < 0) {
+						dir = 3;
+					}
+					int ny = y + dy[dir];
+					int nx = x + dx[dir];
+					if (ny < 0 || ny >= r || nx < 0 || nx >= c) 
+						continue;
+					if (room[ny][nx] == 0) {
+						q.offer(new int[]{ny, nx, dir});
+						break;
+					}
+				}
 			}
 		}
 		System.out.println(ans);
 	}
 
-	static boolean hasDirtyRoom(int sy, int sx) {
+	static boolean isCleaned(int y, int x) {
 
 		for (int dir = 0; dir < 4; ++dir) {
-			int ny = sy + dy[dir];
-			int nx = sx + dx[dir];
-			if (ny < 0 || ny >= r || nx < 0 || nx >= c) continue;
+			int ny = y + dy[dir];
+			int nx = x + dx[dir];
+			if (ny < 0 || ny >= r || nx < 0 || nx >= c) 
+				continue;
 			if (room[ny][nx] == 0)
-				return true;
+				return false;
 		}
-		return false;
+		return true;
 	}
 }
