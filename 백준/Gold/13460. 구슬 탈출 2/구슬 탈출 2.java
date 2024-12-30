a81dd696498ce575fa0d99c8771a5d34105cdb18
ji-jjang <153375785+ji-jjang@users.noreply.github.com>
2024-12-30 15:40:09 +0900

    [Gold I] Title: 구슬 탈출 2, Time: 944 ms, Memory: 18888 KB -BaekjoonHub

--- 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.java
+++ 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.java
@@ -1,134 +1,137 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
 
+	static int r, c;
 	static char[][] board;
-	static int[] dy = {-1, 0, 1, 0};
-	static int[] dx = {0, 1, 0, -1};
-	static int[] red = {0 ,0};
-	static int[] blue = {0, 0};
+	static char[][] copy;
+	static int[] sr, sb;
+	static int[] cr, cb;
 	static int ans = Integer.MAX_VALUE;
-
+	static int[] dy = {-1, 0, 1, 0};
+	static int[] dx = {0, 1, 0 , -1};
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		StringTokenizer st = new StringTokenizer(br.readLine());
-
-		int r = Integer.parseInt(st.nextToken());
-		int c = Integer.parseInt(st.nextToken());
-
+		String[] tokens = br.readLine().split(" ");
+		r = Integer.parseInt(tokens[0]);
+		c = Integer.parseInt(tokens[1]);
 		board = new char[r][c];
+		copy = new char[r][c];
+
 		for (int i = 0; i < r; ++i) {
-			String line = br.readLine();
+			char[] line = br.readLine().toCharArray();
 			for (int j = 0; j < c; ++j) {
-				board[i][j] = line.charAt(j);
-				if (board[i][j] == 'B') {
-					blue[0] = i;
-					blue[1] = j;
+				board[i][j] = line[j];
+				if (board[i][j] == 'R') {
+					sr = new int[]{i, j};
 					board[i][j] = '.';
-				} else if (board[i][j] == 'R') {
-					red[0] = i;
-					red[1] = j;
+				} else if (board[i][j] == 'B') {
+					sb = new int[]{i, j};
 					board[i][j] = '.';
 				}
 			}
 		}
 
-		int[] seq = new int[10];
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < (1 << (2 * 10)); ++i) {
-			int cur = i;
-			for (int j = 0; j < 10; ++j) {
-				seq[j] = cur % 4;
-				cur /= 4;
-			}
-			start(seq);
+		dfs(0, new ArrayList<>());
+
+		if (ans == Integer.MAX_VALUE) {
+			System.out.println(-1);
+			return;
 		}
-		if (ans == Integer.MAX_VALUE)
-			ans = -1;
 		System.out.println(ans);
 	}
 
-	static void start(int[] seq) {
-
-		int ry = red[0];
-		int rx = red[1];
-		int by = blue[0];
-		int bx = blue[1];
-		boolean isEnd = false, isSuccess = false;
-		int rrot = 0;
-		for (int rot = 0; rot < 10; ++rot) {
-			if (rot >= ans)
-				break;
-			isEnd = false;
-			isSuccess = false;
-			int dir = seq[rot];
-
-			int rry = ry;
-			int rrx = rx;
-			int bby = by;
-			int bbx = bx;
-			while (board[rry + dy[dir]][rrx + dx[dir]] == '.') {
-				rry += dy[dir];
-				rrx += dx[dir];
-			}
-			while (board[bby + dy[dir]][bbx + dx[dir]] == '.') {
-				bby += dy[dir];
-				bbx += dx[dir];
-			}
-
-			if (board[rry + dy[dir]][rrx + dx[dir]] == '#' && board[bby + dy[dir]][bbx + dx[dir]] == '#') {
-				if (rry == bby && rrx == bbx) {
-					if (dir == 0) {
-						if (ry > by) {
-							++rry;
-						} else {
-							++bby;
-						}
-					} else if (dir == 1) {
-						if (rx < bx) {
-							--rrx;
-						} else {
-							--bbx;
-						}
-					} else if (dir == 2) {
-						if (ry > by) {
-							--bby;
-						}
-						else {
-							--rry;
-						}
-					} else if (dir == 3) {
-						if (rx > bx) {
-							++rrx;
-						} else {
-							++bbx;
-						}
-					}
+	static void dfs(int depth, List<Integer> selected) {
+		if (depth == 10) {
+			for (int i = 0; i < r; ++i) {
+				for (int j = 0; j < c; ++j) {
+					copy[i][j] = board[i][j];
 				}
-				ry = rry;
-				rx = rrx;
-				by = bby;
-				bx = bbx;
 			}
-			if (board[bby + dy[dir]][bbx + dx[dir]] == 'O') {
-				isEnd = true;
-				break;
+			cr = sr;
+			cb = sb;
+			for (int i = 0; i < selected.size(); ++i) {
+				int ret = move(selected.get(i));
+				if (ret == 0) { // fail
+					break;
+				}
+				else if (ret == 1) { // success
+					ans = Math.min(ans, i + 1);
+				}
+				// 2 continue;
 			}
-			if (board[rry + dy[dir]][rrx + dx[dir]] == 'O') {
-				isSuccess = true;
-				rrot = rot;
-				break;
-			}
-		}
-		if (isEnd) {
 			return;
 		}
-		if (isSuccess) {
-			ans = Math.min(ans, rrot + 1);
-			return ;
+		for (int dir = 0; dir < 4; ++dir) {
+			selected.add(dir);
+			dfs(depth + 1, selected);
+			selected.remove(selected.size() - 1);
 		}
+	}
+
+	static int move(int dir) {
+
+		boolean redArrive = false;
+		boolean blueArrive = false;
+		int ry = cr[0];
+		int rx = cr[1];
+		int by = cb[0];
+		int bx = cb[1];
+		while (copy[ry + dy[dir]][rx + dx[dir]] != '#') {
+			if (copy[ry + dy[dir]][rx + dx[dir]] == 'O') {
+				redArrive = true;
+				break;
+			}
+			ry += dy[dir];
+			rx += dx[dir];
+		}
+
+		while (copy[by + dy[dir]][bx + dx[dir]] != '#') {
+			if (copy[by + dy[dir]][bx + dx[dir]] == 'O') {
+				blueArrive = true;
+				break;
+			}
+			by += dy[dir];
+			bx += dx[dir];
+		}
+		if (redArrive && !blueArrive) {
+			return 1;
+		}
+		else if (!redArrive && blueArrive) {
+			return 0;
+		}
+		else if (redArrive && blueArrive) {
+			return 0;
+		}
+
+		if (ry == by && rx == bx) {
+			if (dir == 0) {
+				if (cr[0] < cb[0])
+					++by;
+				else
+					++ry;
+			} else if (dir == 1) {
+				if (cr[1] < cb[1]) 
+					--rx;
+				else
+					--bx;
+			} else if (dir == 2) {
+				if (cr[0] < cb[0])
+					--ry;
+				else
+					--by;
+			} else if (dir == 3) {
+				if (cr[1] < cb[1]) 
+					++bx;
+				else
+					++rx;
+			}
+		}
+		cr = new int[]{ry, rx};
+		cb = new int[]{by, bx};
+		return 2;
 	}
 }
