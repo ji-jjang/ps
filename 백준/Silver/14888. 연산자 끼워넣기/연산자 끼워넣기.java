49eb8c810e8a0dadb102664a0879be6d550ce62b
ji-jjang <153375785+ji-jjang@users.noreply.github.com>
2025-01-01 13:10:46 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 676 ms, Memory: 19392 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -1,47 +1,68 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int n;
-    static int[] a = new int[14];
-    static int[] oper = {0, 0, 0, 0};
-    static int mx = Integer.MIN_VALUE;
-    static int mn = Integer.MAX_VALUE;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++)
-            a[i] = Integer.parseInt(st.nextToken());
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < 4; i++)
-            oper[i] = Integer.parseInt(st.nextToken());
-        dfs(1, a[0]);
-        System.out.println(mx);
-        System.out.println(mn);
-        br.close();
-    }
 
-    static void dfs(int depth, int sum) {
-        if (depth == n) {
-            mx = Math.max(mx, sum);
-            mn = Math.min(mn, sum);
-        }
-        for (int i = 0; i < 4; ++i) {
-            if (oper[i] == 0)
-                continue;
-            --oper[i];
-            if (i == 0)
-                dfs(depth + 1, sum + a[depth]);
-            else if (i == 1)
-                dfs(depth + 1, sum - a[depth]);
-            else if (i == 2)
-                dfs(depth + 1, sum * a[depth]);
-            else
-                dfs(depth + 1, sum / a[depth]);
-            ++oper[i];
-        }
-    }
+	static int n;
+	static int[] nums; 
+	static List<Integer> opers = new ArrayList<>();
+	static int minValue = Integer.MAX_VALUE;
+	static int maxValue = Integer.MIN_VALUE;
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		n = Integer.parseInt(br.readLine());
+		nums = new int[n];
+
+		String[] tokens = br.readLine().split(" ");
+		for (int i = 0; i < n; ++i) {
+			nums[i] = Integer.parseInt(tokens[i]);
+		}
+		tokens = br.readLine().split(" ");
+		for (int i = 0; i < 4; ++i) {
+			int num = Integer.parseInt(tokens[i]);
+			for (int j = 0; j < num; ++j) {
+				opers.add(i);
+			}
+		}
+
+		dfs(0, new ArrayList<>(), new boolean[n - 1]);
+
+		System.out.println(maxValue + "\n" + minValue);
+	}
+
+	static void dfs(int depth, List<Integer> selected, boolean[] isVisited) {
+		if (depth == n - 1) {
+
+			int ret = nums[0];
+			for (int i = 1; i < n; ++i) {
+				int op = selected.get(i - 1);
+				if (op == 0) {
+					ret += nums[i];
+				}
+				else if (op == 1) {
+					ret -= nums[i];
+				}
+				else if (op == 2) {
+					ret *= nums[i];
+				}
+				else {
+					ret /= nums[i];
+				}
+			}
+			minValue = Math.min(minValue, ret);
+			maxValue = Math.max(maxValue, ret);
+			return;
+		}
+
+		for (int i = 0; i < opers.size(); ++i) {
+			if (isVisited[i]) continue;
+			isVisited[i] = true;
+			selected.add(opers.get(i));
+			dfs(depth + 1, selected, isVisited);
+			isVisited[i] = false;
+			selected.remove(selected.size() - 1);
+		}
+	}
 }
